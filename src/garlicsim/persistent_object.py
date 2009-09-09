800c4d635219911c30f26ab6aea17d6f2bc1e3b1
Ram Rachum <cool-rr@cool-rr.com>
2009-09-09 17:04:02 +0200

    booga

--- src/garlicsim/persistent_object.py
+++ src/garlicsim/persistent_object.py
@@ -1,4 +1,44 @@
-import copy
-copy.
+import gc
+
+
+library = []
+
+
+def get_object_by_id(id_):
+    for thing in gc.get_objects():
+        if id(thing) == id_:
+            return thing
+    raise Exception("No found")
+
 class PersistentObject(object):
-    pass
+    """
+    def __init__(self):
+    """
+    
+    def __getstate__(self):
+        return (id(self), self.__dict__)
+    
+    def __new__(cls, *args):
+        if args:
+            return FFF
+        else:
+            thing = object.__new__(cls)
+            library.append(id(thing))
+            thing
+    
+    def __getnewargs__(self):
+        return id(self)
+    
+    def __setstate__(self, state):
+        
+        pass
+    
+    
+    def __deepcopy__(self, memo):
+        return self
+    
+    def __copy__(self):
+        return self
+    
+    def __del__(self):
+        library.remove(id(self))
