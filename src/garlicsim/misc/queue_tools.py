9f81c5e27c3ec0936a8b2de5a5c9c232d57b96ab
Ram Rachum <cool-rr@cool-rr.com>
2009-10-09 01:14:45 +0200

    arranged step_options_profile

--- src/garlicsim/misc/queue_tools.py
+++ src/garlicsim/misc/queue_tools.py
@@ -8,20 +8,24 @@ when working with queues.
 
 import Queue
 
-
 def dump_queue(queue):
     """
-    Empties all pending items in a queue
-    and returns them in a list.
+    Empties all pending items in a queue and returns them in a list.
     """
     result = []
+
+    # START DEBUG CODE
     initial_size = queue.qsize()
     print("Queue has %s items initially." % initial_size)
+    #  END  DEBUG CODE
+    
     while True:
         try:
             thing = queue.get(block=False)
             result.append(thing)
         except Queue.Empty:
+            
+            # START DEBUG CODE
             current_size = queue.qsize()
             total_size = current_size + len(result)
             print("Dumping complete:")
@@ -30,8 +34,10 @@ def dump_queue(queue):
             else:
                 print("%s items were added to the queue." % \
                       (total_size - initial_size))
-            print("Extracted %s items from the queue, queue has %s items left" \
-                  % (len(result), current_size))
+            print("Extracted %s items from the queue, queue has %s items \
+            left" % (len(result), current_size))
+            #  END  DEBUG CODE
+            
             return result
 
 
--- src/garlicsim/step_options_profile.py
+++ src/garlicsim/step_options_profile.py
@@ -1,10 +1,24 @@
-class ArgumentsProfile(object):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-    def __eq__(self, other):
-        return isinstance(other, ArgumentsProfile) and \
-               self.args == other.args and self.kwargs == other.kwargs
+"""
+This module defines the ArgumentsProfile class. See its documentation for
+more information.
+"""
+
+import garlicsim.misc.arguments_profile
                
-class StepOptionsProfile(ArgumentsProfile):
-    pass
+class StepOptionsProfile(garlicsim.misc.arguments_profile.ArgumentsProfile):
+    """
+    A profile of *args and **kwargs to be used with a step function.
+    
+    Usage:
+    step_options_profile = StepOptionsProfile(34, "meow", width=60)
+    step(state, *arguments_profile.args, **arguments_profile.kwargs)
+    
+    """
+    def __repr__(self):
+        args_string = ', '.join([repr(thing) for thing in self.args])
+        kwargs_string = ', '.join([str(key)+'='+repr(value) for \
+                                   (key, value) in self.kwargs.items()])
+        strings = [thing for thing in [args_string, kwargs_string] if \
+                   thing]        
+        big_string = ', '.join(strings)
+        return 'StepOptionsProfile(%s)' % big_string
