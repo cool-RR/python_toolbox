e0809fa766f44c2ce819cd7abc7a311c3f7c9e8e
Ram Rachum <cool-rr@cool-rr.com>
2009-10-10 15:46:06 +0200

    psyco

--- src/run_demo.py
+++ src/run_demo.py
@@ -1,5 +1,15 @@
 import garlicsim
 import garlicsim_wx
 
-if __name__=="__main__":
+try:
+    import psyco
+    psyco_installed = True
+except ImportError:
+    psyco_installed = False
+
+if __name__ == '__main__':
+    
+    if psyco_installed:
+        pass # psyco.full()
+        
     garlicsim_wx.main()
