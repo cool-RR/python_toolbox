c05be76bcbabd255a7fa2cd7bc7f0aa7ecd54146
Ram Rachum <cool-rr@cool-rr.com>
2009-10-14 15:08:23 +0200

    -

--- src/garlicsim/general_misc/junk/test4.py
+++ src/garlicsim/general_misc/junk/test4.py
@@ -2,14 +2,13 @@ import time
 import garlicsim
 from garlicsim_wx.simulation_packages import life
 
-if __name__ == '__main__':
-    s = life.make_random_state()
-    p = garlicsim.Project(life)
-    n = p.root_this_state(s)
-    p.crunching_manager.Cruncher = \
-        garlicsim.asynchronous_crunching.crunchers.CruncherThread
-    p.crunch_all_leaves(n, 100)
-    p.sync_crunchers()
-    time.sleep(10)
-    p.sync_crunchers()
+state = life.make_random_state(40, 15)
+new_state = garlicsim.simulate(life, state)
+
+p = garlicsim.Project(life)
+n = p.root_this_state(s)
+p.crunch_all_leaves(n, 100)
+p.sync_crunchers()
+time.sleep(10)
+p.sync_crunchers()
     
