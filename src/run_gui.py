dc88518686aa81acf49d537850486962ed80c49d
Ram Rachum <cool-rr@cool-rr.com>
2009-10-18 10:34:55 +0200

    -

--- src/run_gui.py
+++ src/run_gui.py
@@ -1,15 +1,21 @@
+import sys
 import garlicsim
 import garlicsim_wx
 
-try:
-    import psyco
-    psyco_installed = True
-except ImportError:
-    psyco_installed = False
+arguments = sys.argv[1:]
+debug = '-debug' in arguments
 
+use_psyco = False
+if not debug:
+    try:
+        import psyco
+        use_psyco = True
+    except ImportError:
+        pass
+    
 if __name__ == '__main__':
     
-    if psyco_installed:
-        pass # psyco.full()
+    if use_psyco:
+        psyco.full()
         
     garlicsim_wx.main()
