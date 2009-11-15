2603dabb82aad539e655639e449e90c4836114db
Ram Rachum <cool-rr@cool-rr.com>
2009-11-15 17:56:19 +0200

    -

--- garlicsim/garlicsim/asynchronous_crunching/crunching_manager.py
+++ garlicsim/garlicsim/asynchronous_crunching/crunching_manager.py
@@ -224,6 +224,9 @@ class CrunchingManager(object):
                     parent=current,
                     step_profile=self.step_profiles[cruncher]
                 )
+                # todo optimization: save step profile in variable, it's
+                # wasteful to do a dict lookup every state.
+                
             elif isinstance(thing, StepProfile):
                 self.step_profiles[cruncher] = thing
             else:
