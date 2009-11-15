4d425929303f555f0a66e74bcf55e71ec4b1e319
Ram Rachum <cool-rr@cool-rr.com>
2009-11-15 18:28:00 +0200

    -

--- garlicsim/garlicsim/__init__.py
+++ garlicsim/garlicsim/__init__.py
@@ -16,8 +16,51 @@ This licensing does not apply to `garlicsim_wx`, which is the associated GUI
 package.
 '''
 
-import general_misc.requirement_checker
-general_misc.requirement_checker.require('Distribute')
+
+
+###############################################################################
+###  Checking for prerequisites:
+###############################################################################
+
+def __check_prerequisites():
+    '''
+    Check that all modules required for garlicsim are installed.
+    
+    Returns a list of some imported modules: A reference to this list should be
+    kept alive so to prevent the imported modules from being garbage-collected,
+    which would cause Python to load them twice, which would needlessly increase
+    startup time.
+    '''
+    
+    modules = []
+    
+    class MissingModule(Exception):
+        '''An error to raise when a required module is not found.'''
+        pass
+    
+    def check_distribute():
+        try:
+            import pkg_resources
+            modules.append(pkg_resources)
+            assert pkg_resources.require('Distribute >= 0.6')
+        except (ImportError, pkg_resources.DistributionNotFound, AssertionError):
+            raise MissingModule('''Distribute (version 0.6 and upwards) is \
+required, but it's not currently installed on your system. Please find it \
+online and install it, then try again.''')
+        return [pkg_resources]
+    
+    modules += check_distribute()
+    
+    return modules
+
+__modules_list = __check_prerequisites()
+
+
+
+
+###############################################################################
+###  Importing:
+###############################################################################
 
 import general_misc
 import misc
--- garlicsim_wx/garlicsim_wx/__init__.py
+++ garlicsim_wx/garlicsim_wx/__init__.py
@@ -9,9 +9,71 @@ working with simulations, friendly enough that it may be used by
 non-programmers.
 '''
 
-import garlicsim.general_misc.requirement_checker
-for thing in ['Distribute']: # + ['GarlicSim', 'wxPython'] arrgh : # cancel in my debug mode?
-    garlicsim.general_misc.requirement_checker.require(thing)
+###############################################################################
+###  Checking for prerequisites:
+###############################################################################
+
+def __check_prerequisites():
+    '''
+    Check that all modules required for garlicsim_wx are installed.
+    
+    Returns a list of some imported modules: A reference to this list should be
+    kept alive so to prevent the imported modules from being garbage-collected,
+    which would cause Python to load them twice, which would needlessly increase
+    startup time.
+    '''
+    
+    modules = []
+    
+    class MissingModule(Exception):
+        '''An error to raise when a required module is not found.'''
+        pass
+    
+    def check_garlicsim():
+        try:
+            import garlicsim
+            return [garlicsim]
+        except ImportError:
+            raise MissingModule('''garlicsim is \
+required, but it's not currently installed on your system. Please find it \
+online and install it, then try again.''')
+        
+    
+    def check_distribute():
+        try:
+            import pkg_resources
+            modules.append(pkg_resources)
+            assert pkg_resources.require('Distribute >= 0.6')
+        except (ImportError, pkg_resources.DistributionNotFound, AssertionError):
+            raise MissingModule('''Distribute (version 0.6 and upwards) is \
+required, but it's not currently installed on your system. Please find it \
+online and install it, then try again.''')
+        return [pkg_resources]
+    
+    def check_wx():
+        try:
+            import wx
+            return [wx]
+        except ImportError:
+            raise MissingModule('''wxPython is \
+required, but it's not currently installed on your system. Please find it \
+online and install it, then try again.''')
+    
+
+    checkers = [check_distribute, check_garlicsim, check_wx]
+    
+    for checker in checkers:
+        modules += checker()
+    
+    return modules
+
+__modules_list = __check_prerequisites()
+
+########################## Done checking for prerequisites
+
+
+
+
 
 import wx
 
