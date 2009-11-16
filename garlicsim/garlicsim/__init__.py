a949137c5aca0641d296271a8733137723579bbd
Ram Rachum <cool-rr@cool-rr.com>
2009-11-16 03:42:56 +0200

    -roar!

--- garlicsim/garlicsim/__init__.py
+++ garlicsim/garlicsim/__init__.py
@@ -16,52 +16,7 @@ This licensing does not apply to `garlicsim_wx`, which is the associated GUI
 package.
 '''
 
-
-
-###############################################################################
-###  Checking for prerequisites:
-###############################################################################
-
-def __check_prerequisites():
-    '''
-    Check that all modules required for garlicsim are installed.
-    
-    Returns a list of some imported modules: A reference to this list should be
-    kept alive so to prevent the imported modules from being garbage-collected,
-    which would cause Python to load them twice, which would needlessly increase
-    startup time.
-    '''
-    
-    modules = []
-    
-    class MissingModule(Exception):
-        '''An error to raise when a required module is not found.'''
-        pass
-    
-    def check_distribute():
-        try:
-            import pkg_resources
-            modules.append(pkg_resources)
-            assert pkg_resources.require('Distribute >= 0.6')
-        except (ImportError, pkg_resources.DistributionNotFound, AssertionError):
-            raise MissingModule('''Distribute (version 0.6 and upwards) is \
-required, but it's not currently installed on your system. Please find it \
-online and install it, then try again.''')
-        return [pkg_resources]
-    
-    modules += check_distribute()
-    
-    return modules
-
-__modules_list = __check_prerequisites()
-
-
-
-
-###############################################################################
-###  Importing:
-###############################################################################
-
+import bootstrap
 import general_misc
 import misc
 from asynchronous_crunching import Project
---
+++ garlicsim/garlicsim/bootstrap/bootstrap.py
@@ -0,0 +1,41 @@
+# Copyright 2009 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+This is a bootstrap module for garlicsim.
+
+It checks all prerequisites are installed.
+'''
+
+def __check_prerequisites():
+    '''
+    Check that all modules required for garlicsim are installed.
+    
+    Returns a list of some imported modules: A reference to this list should be
+    kept alive so to prevent the imported modules from being garbage-collected,
+    which would cause Python to load them twice, which would needlessly increase
+    startup time.
+    '''
+    
+    modules = []
+    
+    class MissingModule(Exception):
+        '''An error to raise when a required module is not found.'''
+        pass
+    
+    def check_distribute():
+        try:
+            import pkg_resources
+            modules.append(pkg_resources)
+            assert pkg_resources.require('Distribute >= 0.6')
+        except (ImportError, pkg_resources.DistributionNotFound, AssertionError):
+            raise MissingModule('''Distribute (version 0.6 and upwards) is \
+required, but it's not currently installed on your system. Please find it \
+online and install it, then try again.''')
+        return [pkg_resources]
+    
+    modules += check_distribute()
+    
+    return modules
+
+__modules_list = __check_prerequisites()
