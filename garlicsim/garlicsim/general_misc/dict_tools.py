2fd36559c2a6986c00117ff4235c99d4d7d9a847
Ram Rachum <cool-rr@cool-rr.com>
2009-11-21 07:01:26 +0200

    cooler dict shower

--- garlicsim/garlicsim/general_misc/dict_tools.py
+++ garlicsim/garlicsim/general_misc/dict_tools.py
@@ -23,10 +23,24 @@ def deepcopy_values(d):
 
 def fancy_string(d, indent=0):
     '''Show a dict as a string, slightly nicer than dict.__repr__.'''
+
     small_space = ' ' * indent
+    
     big_space = ' ' * (indent + 4)
-    temp1 = ((big_space + str(key) + ': ' + str(value)) for (key, value) in d.items())
+    
+    huge_space = ' ' * (indent + 8)
+    
+    def show(thing, indent=0):
+        space = ' ' * indent
+        enter_then_space = '\n' + space
+        return repr(thing).replace('\n', enter_then_space)
+    
+    temp1 = (
+        (big_space + repr(key) + ':\n' + huge_space + show(value, indent + 8))
+        for(key, value) in d.items())
+    
     temp2 = small_space + '{\n' + ',\n'.join(temp1) + '\n' + small_space +'}'
+    
     return temp2
     
     
