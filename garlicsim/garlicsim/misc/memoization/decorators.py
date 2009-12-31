19c476902e1ea26c75ebfe8c656152c53ef74704
Ram Rachum <cool-rr@cool-rr.com>
2009-12-31 22:11:16 +0200

    -

--- garlicsim/garlicsim/misc/memoization/decorators.py
+++ garlicsim/garlicsim/misc/memoization/decorators.py
@@ -6,3 +6,13 @@ tododoc
 '''
 
 from garlicsim.general_misc.third_party import decorator as decorator_module
+
+def state_memoize(function, *args, **kwargs):
+    if not hasattr(function, 'memo'):
+        function.memo = {}
+    
+    if 
+    pass
+
+def history_memoize(function, *args, **kwargs):
+    pass
