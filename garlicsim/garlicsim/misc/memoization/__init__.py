eadb134a53649d863fe810378a74883d9148259c
Ram Rachum <cool-rr@cool-rr.com>
2009-12-31 21:45:46 +0200

    -

---
+++ garlicsim/garlicsim/misc/memoization/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2009-2010 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+tododoc
+'''
+
+from decorators import *
+from abcs import *
---
+++ garlicsim/garlicsim/misc/memoization/decorators.py
@@ -0,0 +1,8 @@
+# Copyright 2009-2010 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+tododoc
+'''
+
+from garlicsim.general_misc.third_party import decorator as decorator_module
