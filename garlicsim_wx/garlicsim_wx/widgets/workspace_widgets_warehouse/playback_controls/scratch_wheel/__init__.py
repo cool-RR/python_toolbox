1d3c993e5664bad49cf3cfda61be8cc7db435ac5
Ram Rachum <cool-rr@cool-rr.com>
2010-03-31 05:53:57 +0200

    Starting ScratchWheel

---
+++ garlicsim_wx/garlicsim_wx/widgets/workspace_widgets_warehouse/playback_controls/scratch_wheel/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2009-2010 Ram Rachum. No part of this program may be used, copied
+# or distributed without explicit written permission from Ram Rachum.
+
+'''
+tododoc
+'''
+
+from scratch_wheel import ScratchWheel
+
+__all__ = ["ScratchWheel"]
