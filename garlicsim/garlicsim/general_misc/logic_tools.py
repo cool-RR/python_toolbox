7f4a8bfc8ea01738372160c06cd9f2fd35f189c3
Ram Rachum <cool-rr@cool-rr.com>
2010-04-21 23:30:38 +0200

    -

--- garlicsim/garlicsim/general_misc/logic_tools.py
+++ garlicsim/garlicsim/general_misc/logic_tools.py
@@ -8,12 +8,22 @@ This module defines logic-related tools.
 import garlicsim.general_misc.cute_iter_tools as cute_iter_tools
 
 
-def all_equal(iterable):
+def all_equal(iterable, exhaustive=False):
     '''
     Return whether all elements in the iterable are equal to each other.
     
-    It is assumed that the equality relation is transitive, therefore not every
-    member is tested against every other member. In a list of size n, n-1
-    equality checks will be made.
+    If `exhaustive` is set to False, it's assumed that the equality relation is
+    transitive, therefore not every member is tested against every other member.
+    So in a list of size n, n-1 equality checks will be made.
+    
+    If `exhaustive` is set to True, every member will be checked against every
+    other member. So in a list of size n, (n*(n-1))/2 equality checks will be
+    made.
     '''
-    return all(a==b for (a, b) in cute_iter_tools.consecutive_pairs(iterable))
+    
+    if exhaustive is True:
+        pairs = cute_iter_tools.orderless_combinations(iterable, 2)        
+    else: # exhaustive is False
+        pairs = cute_iter_tools.consecutive_pairs(iterable)
+        
+    return all(a==b for (a, b) in pairs)
