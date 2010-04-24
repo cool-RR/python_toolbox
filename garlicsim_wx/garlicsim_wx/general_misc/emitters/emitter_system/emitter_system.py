2fc72f03576d3e6aeff7aea25a1e34ac7429757a
Ram Rachum <cool-rr@cool-rr.com>
2010-04-24 19:01:30 +0200

    -

--- garlicsim_wx/garlicsim_wx/general_misc/emitters/emitter_system/emitter_system.py
+++ garlicsim_wx/garlicsim_wx/general_misc/emitters/emitter_system/emitter_system.py
@@ -14,18 +14,45 @@ from garlicsim.general_misc import cute_iter_tools
 
 from emitter import Emitter
 
+
 class FreezeCacheRebuildingContextManager(object):
+    '''
+    Context manager for freezing the cache rebuilding in an emitter system.
+    
+    When you do actions using this context manager, the emitters will not
+    rebuild their cache when changing their inputs/outputs. When the outermost
+    context manager has exited, all the caches for these emitters will get
+    rebuilt.
+    '''    
     def __init__(self, emitter_system):
         self.emitter_system = emitter_system
+        
     def __enter__(self):
         assert self.emitter_system._cache_rebuilding_frozen >= 0
         self.emitter_system._cache_rebuilding_frozen += 1
+        
     def __exit__(self, *args, **kwargs):
         self.emitter_system._cache_rebuilding_frozen -= 1
         if self.emitter_system._cache_rebuilding_frozen == 0:
             self.emitter_system._recalculate_all_cache()
 
+            
 class EmitterSystem(object):
+    '''
+    A system of emitters, representing a set of possible events in a program
+    
+    EmitterSystem offers a few advantages over using plain emitters.
+    
+    There are the `bottom_emitter` and `top_emitter`, which allow, respectively,
+    to keep track of each `emit`ting that goes on, and to generate an `emit`ting
+    that affects all emitters in the system.
+    
+    The EmitterSystem also offers a context manager, `.freeze_cache_rebuilding`.
+    When you do actions using this context manager, the emitters will not
+    rebuild their cache when changing their inputs/outputs. When the outermost
+    context manager has exited, all the caches for these emitters will get
+    rebuilt.
+    '''
     # possible future idea: there is the idea of optimizing by cutting redundant
     # links between boxes. I'm a bit suspicious of it. The next logical step is
     # to make inputs and outputs abstract.
@@ -49,6 +76,7 @@ class EmitterSystem(object):
         
             
     def make_emitter(self, inputs=(), outputs=(), name=None):
+        '''Create an emitter in this emitter system. Returns the emitter.'''
 
         inputs = set(inputs)
         inputs.add(self.top_emitter)
@@ -59,11 +87,15 @@ class EmitterSystem(object):
         return emitter
     
     def remove_emitter(self, emitter):
+        '''
+        Remove an emitter from this system, disconnecting it from everything.
+        '''
         with self.freeze_cache_rebuilding:
             emitter.disconnect_from_all()
         self.emitters.remove(emitter)
         
     def _recalculate_all_cache(self):
+        '''Recalculate the cache for all the emitters.'''
         self.bottom_emitter._recalculate_total_callable_outputs_recursively()
     
 
