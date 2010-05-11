24603c8320f976c20c32ba4953fac02babdc360d
Ram Rachum <cool-rr@cool-rr.com>
2010-05-11 19:58:33 +0200

    -

--- garlicsim_wx/garlicsim_wx/general_misc/cute_timer.py
+++ garlicsim_wx/garlicsim_wx/general_misc/cute_timer.py
@@ -11,5 +11,5 @@ class CuteTimer(wx.Timer, cute_base_timer.CuteBaseTimer):
     # todo: Should use PyTimer?
     def __init__(self, parent=None, id=wx.ID_ANY):
         wx.Timer.__init__(self, parent, id)
-        CuteBaseTimer.__init__(self, parent)
+        cute_base_timer.CuteBaseTimer.__init__(self, parent)
         
