90b3861783eaed901be99dedc4e120df080fa70c
Ram Rachum <cool-rr@cool-rr.com>
2010-05-12 01:17:12 +0200

    -

--- garlicsim/garlicsim/misc/simpack_grokker/simpack_grokker.py
+++ garlicsim/garlicsim/misc/simpack_grokker/simpack_grokker.py
@@ -28,10 +28,20 @@ class SimpackGrokker(object):
         '''Analyze the simpack.'''
         
         simpack = self.simpack
+        
+        try:
+            State = simpack.State
+        except AttributeError:
+            raise InvalidSimpack('''The %s simpack does not define a State \
+class.''' % simpack.__name__)
+        
+        if not issubclass(State, garlicsim.data_structures.State):
+            raise InvalidSimpack('''The %s simpack define a State class, but \
+it's not a subclass of garlicsim.data_structures.State.''' % simpack.__name__)
 
-        self.simple_non_history_step_defined = hasattr(simpack, "step")
+        self.simple_non_history_step_defined = hasattr(State, "step")
         self.non_history_step_generator_defined = \
-            hasattr(simpack, "step_generator")
+            hasattr(State, "step_generator")
         self.simple_history_step_defined = hasattr(simpack, "history_step")
         self.history_step_generator_defined = hasattr(simpack,
                                                       "history_step_generator")
@@ -51,13 +61,13 @@ class SimpackGrokker(object):
              self.history_step_generator_defined)
         
         if self.history_step_defined and self.non_history_step_defined:
-            raise InvalidSimpack('''The simulation package is defining both a \
+            raise InvalidSimpack('''The %s simpack is defining both a \
 history-dependent step and a non-history-dependent step - which is forbidden.\
-''')
+''' % simpack.__name__)
         
         if not (self.simple_step_defined or self.step_generator_defined):
-            raise InvalidSimpack('''The simulation package has not defined any \
-kind of step function.''')
+            raise InvalidSimpack('''The %s simpack has not defined any kind \
+of step function.''' % simpack.__name__)
         
         self.history_dependent = self.history_step_defined
         
@@ -113,14 +123,14 @@ kind of step function.''')
 
         if self.simple_step_defined:
             step_function = self.simpack.history_step if \
-                          self.history_dependent else self.simpack.step
+                          self.history_dependent else self.simpack.State.step
             result = step_function(state_or_history_browser,
                                    *step_profile.args,
                                    **step_profile.kwargs)
         else: # self.step_generator_defined is True
             step_generator = self.simpack.history_step_generator if \
                           self.history_dependent else \
-                          self.simpack.step_generator
+                          self.simpack.State.step_generator
             iterator = step_generator(state_or_history_browser,
                                       *step_profile.args,
                                       **step_profile.kwargs)
@@ -142,13 +152,13 @@ kind of step function.''')
         if self.step_generator_defined:
             step_generator = self.simpack.history_step_generator if \
                            self.history_dependent else \
-                           self.simpack.step_generator
+                           self.simpack.State.step_generator
             return StepIterator(state_or_history_browser, step_profile,
                                 step_generator=step_generator)
         else:
             assert self.simple_step_defined
             simple_step = self.simpack.history_step if self.history_dependent \
-                        else self.simpack.step
+                        else self.simpack.State.step
             return StepIterator(state_or_history_browser, step_profile,
                                 simple_step=simple_step)
         
