73c52198ebc6ac8130afb7079ebed37bec170b20
Ram Rachum <cool-rr@cool-rr.com>
2010-05-14 20:48:38 +0200

    -

--- garlicsim/garlicsim/scripts/simpack_template/bla.py
+++ garlicsim/garlicsim/scripts/simpack_template/bla.py
@@ -1,17 +1,7 @@
-#!/usr/bin/env python
-
-import os
-import sys
 import re
 import shutil
 
-import pkg_resources
-
-import simpack_template
-simpack_template_module_name = simpack_template.__name__
-
-
-def start_simpack(containing_folder, name):
+def start_simpack(directory, name):
     """
     
     """
@@ -23,12 +13,12 @@ def start_simpack(containing_folder, name):
         else:
             message = 'use only numbers, letters and underscores'
         raise Exception("%r is not a valid simpack name. Please %s." % (name, app_or_project, message))
-    folder = os.path.join(containing_folder, name)
-    
-    os.mkdir(folder)
+    top_dir = os.path.join(directory, name)
+    try:
+        os.mkdir(top_dir)
+    except OSError, e:
+        raise Exception(e)
 
-    CODE A WALK IN PKG_RESOURCES
-    
     # Determine where the app or project templates are. Use
     # django.__path__[0] because we don't know into which directory
     # django has been installed.
@@ -59,7 +49,6 @@ def start_simpack(containing_folder, name):
             except OSError:
                 sys.stderr.write(style.NOTICE("Notice: Couldn't set permission bits on %s. You're probably using an uncommon filesystem setup. No problem.\n" % path_new))
 
-                
 def _make_writeable(filename):
     """
     Make sure that the file is writeable. Useful if our source is
@@ -74,22 +63,3 @@ def _make_writeable(filename):
         st = os.stat(filename)
         new_permissions = stat.S_IMODE(st.st_mode) | stat.S_IWUSR
         os.chmod(filename, new_permissions)
-
-        
-def show_help():
-    '''tododoc'''
-    
-if __name__ == '__main__':
-
-    if len(sys.argv) != 2:
-        show_help()
-        sys.exit()
-    arg = sys.argv[1]
-
-    if arg == '--help':
-        show_help()
-        sys.exit()
-    
-    start_simpack(os.curdir, arg)
-    
-        
