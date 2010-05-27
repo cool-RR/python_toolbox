b69c7cf3a720b264c47a3489b91395f2fb90976b
Ram Rachum <cool-rr@cool-rr.com>
2010-05-27 14:45:33 +0200

    -

--- garlicsim/garlicsim/scripts/start_simpack.bat
+++ garlicsim/garlicsim/scripts/start_simpack.bat
@@ -1,7 +1,26 @@
 @echo off
 
-xcopy ""%~dp0"\simpack_template\simpack_name" %1 /E/Q/K/I
+if "%1" == "" goto help
+
+if "%1" == "help" (
+	:help
+	echo.This is a script for creating a skeleton for a garlicsim simpack. Use this when
+    echo.you want to make a new simpack to have the basic folders and files created for
+    echo.you.
+    echo.
+    echo.    Usage: start_simpack.py my_simpack_name
+    echo.
+    echo.The simpack will be created in the current path, in a directory with the name of
+    echo.the simpack.
+	goto end
+)
+
+xcopy "%~dp0\simpack_template\simpack_name" %1 /E/Q/K/I
 del /S/Q "%1\*.pyc"
 del /S/Q "%1\*.pyo"
 
+echo. %1 simpack created successfully! Explore the %1 folder and start
+echo. filling in the contents of your new simpack.
+
+:end
 @echo on
