58dba9cbaa5f9ec2db995f098d11a267c9186d5b
Ram Rachum <cool-rr@cool-rr.com>
2010-07-25 17:32:59 +0200

    -

--- garlicsim/garlicsim/synchronous_crunching/iter_simulate.py
+++ garlicsim/garlicsim/synchronous_crunching/iter_simulate.py
@@ -2,7 +2,7 @@
 # This program is distributed under the LGPL2.1 license.
 
 '''
-This module defines the list_simulate function.
+This module defines the iter_simulate function.
 
 See its documentation for more info.
 '''
@@ -16,18 +16,23 @@ from garlicsim.general_misc import misc_tools
 import garlicsim
 import garlicsim.misc
 import history_browser as history_browser_module # Avoiding name clash
+from .list_simulate import _history_list_simulate
 
-__all__ = ["list_simulate"]
+
+__all__ = ["iter_simulate"]
 
 
-def list_simulate(state, iterations, *args, **kwargs):
+def iter_simulate(state, iterations, *args, **kwargs):
     '''
     Simulate from the given state for the given number of iterations.
 
     Any extraneous parameters will be passed to the step function.
     
-    Returns a list that spans all the states, from the initial one given to
-    the final one.
+    This returns a generator that yields all the states one-by-one, from the
+    initial state to the final one.
+    
+    (Note that in the case of history-dependent simulations, all states must be
+    stored anyway, so this function will return a list.)
     '''
     simpack_grokker = garlicsim.misc.SimpackGrokker.create_from_state(state)
     step_profile = garlicsim.misc.StepProfile(*args, **kwargs)
@@ -41,50 +46,11 @@ def list_simulate(state, iterations, *args, **kwargs):
         return _history_list_simulate(simpack_grokker, state, iterations,
                                        step_profile)
     else: # It's a non-history-dependent simpack
-        return _non_history_list_simulate(simpack_grokker, state, iterations,
+        return _non_history_iter_simulate(simpack_grokker, state, iterations,
                                            step_profile)
+    
 
-    
-def _history_list_simulate(simpack_grokker, state, iterations,
-                             step_profile=None):
-    '''
-    Simulate from the given state for the given number of iterations.
-
-    (Internal function for history-dependent simulations only.)    
-    
-    A simpack grokker must be passed as the first parameter. Any extraneous
-    parameters will be passed to the step function.
-    
-    Returns a list that spans all the states, from the initial one given to
-    the final one.
-    '''
-    
-    if step_profile is None: step_profile = garlicsim.misc.StepProfile()
-    
-    tree = garlicsim.data_structures.Tree()
-    root = tree.add_state(state, parent=None)
-    path = root.make_containing_path()
-    history_browser = history_browser_module.HistoryBrowser(path)
-    
-    iterator = simpack_grokker.step_generator(history_browser, step_profile)
-    finite_iterator = cute_iter_tools.shorten(iterator, iterations)
-    
-    current_node = root
-    current_state = current_node.state
-    
-    world_ended = False
-    try:
-        for current_state in finite_iterator:
-            current_node = tree.add_state(current_state, parent=current_node)
-    except garlicsim.misc.WorldEnd:
-        world_ended = True
-    
-    # Not doing anything with `world_ended` yet
-        
-    return [node.state for node in path]
-
-
-def _non_history_list_simulate(simpack_grokker, state, iterations,
+def _non_history_iter_simulate(simpack_grokker, state, iterations,
                                 step_profile=None):
     '''
     Simulate from the given state for the given number of iterations.
@@ -94,30 +60,29 @@ def _non_history_list_simulate(simpack_grokker, state, iterations,
     A simpack grokker must be passed as the first parameter. Any extraneous
     parameters will be passed to the step function.
     
+    This returns a generator that yields all the states one-by-one, from the
+    initial state to the final one.
+    
     Returns a list that spans all the states, from the initial one given to
     the final one.
     '''
 
     if step_profile is None: step_profile = garlicsim.misc.StepProfile()
     
-    tree = garlicsim.data_structures.Tree()
-    root = tree.add_state(state, parent=None)
-    path = root.make_containing_path()
-    
     iterator = simpack_grokker.step_generator(state, step_profile)
     finite_iterator = cute_iter_tools.shorten(iterator, iterations)
     
-    current_node = root
-    current_state = current_node.state
+    current_state = state
+    
+    yield current_state
     
     world_ended = False
     try:
         for current_state in finite_iterator:
-            current_node = tree.add_state(current_state, parent=current_node)
+            yield current_state
     except garlicsim.misc.WorldEnd:
         world_ended = True
 
     # Not doing anything with `world_ended` yet
     
-    return [node.state for node in path]
-
+    raise StopIteration
