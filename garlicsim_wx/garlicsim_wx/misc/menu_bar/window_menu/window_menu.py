9fd297907321a7134d2850c11c2defbaaba7cc41
Ram Rachum <cool-rr@cool-rr.com>
2010-08-03 01:24:02 +0200

    -

--- garlicsim_wx/garlicsim_wx/misc/menu_bar/window_menu/window_menu.py
+++ garlicsim_wx/garlicsim_wx/misc/menu_bar/window_menu/window_menu.py
@@ -44,68 +44,64 @@ on the screen'''
             -1,
             '&Crunching',
             ''' Show/hide the crunching tool, which lets you control how your \
-simulation is crunched''',
-             wx.ITEM_CHECK
+simulation is crunched'''
         )       
-        self.crunching_button.Enable(False)
+        #self.crunching_button.Enable(False)
+        
+        self.Bind(wx.EVT_BUTTON, self.on_crunching, source=self.crunching_button)
         
         
         self.local_nodes_examiner_button = self.Append(
             -1,
             '&Local nodes examiner',
             ''' Show/hide the local nodes examiner, which lets you manipulate \
-tree nodes one-by-one''',
-            wx.ITEM_CHECK
+tree nodes one-by-one'''
         )       
-        self.local_nodes_examiner_button.Enable(False)
+        #self.local_nodes_examiner_button.Enable(False)
         
         
         self.playback_controls_button = self.Append(
             -1,
             '&Playback',
             ''' Show/hide the playback controls, which let you control the \
-onscreen playback of the simulation''',
-            wx.ITEM_CHECK
+onscreen playback of the simulation'''
         )       
-        self.playback_controls_button.Enable(False)
+        #self.playback_controls_button.Enable(False)
         
         
         self.seek_bar_button = self.Append(
             -1,
             'Seek-&bar',
-            ''' Show/hide the seek-bar, which lets you navigate the active \
-timeline''',
-            wx.ITEM_CHECK
+            ''' Show/hide the seek-bar, which lets you navigate the active tododoc\
+timeline'''
         )       
-        self.seek_bar_button.Enable(False)
+        #self.seek_bar_button.Enable(False)
         
         
         self.shell_button = self.Append(
             -1,
             '&Shell',
             ''' Show/hide the shell, which lets you analyze your simulation \
-using arbitrary Python code''',
-            wx.ITEM_CHECK
+using arbitrary Python code'''
         )       
-        self.shell_button.Enable(False)
+        #self.shell_button.Enable(False)
         
         
         self.toolbox_button = self.Append(
             -1,
             'Toolbo&x',
             ''' Show/hide the toolbox, in which you can choose between \
-different tools to use in the other widgets''',
-            wx.ITEM_CHECK
+different tools to use in the other widgets'''
         )       
-        self.toolbox_button.Enable(False)
+        #self.toolbox_button.Enable(False)
         
         
         self.tree_browser_button = self.Append(
             -1,
             '&Tree browser',
             ''' Show/hide the tree browser, which lets you navigate the time \
-tree''',
-            wx.ITEM_CHECK
+tree'''
         )       
-        self.tree_browser_button.Enable(False)
-        
+        #self.tree_browser_button.Enable(False)
+    
+    def on_crunching(self, event)
