3a99856328d256b34b6104293a911595e24228fb
Ram Rachum <cool-rr@cool-rr.com>
2010-08-24 03:21:34 +0200

    -

--- garlicsim/garlicsim/general_misc/arguments_profile.py
+++ garlicsim/garlicsim/general_misc/arguments_profile.py
@@ -2,7 +2,7 @@
 # This program is distributed under the LGPL2.1 license.
 
 from garlicsim.general_misc.third_party import inspect
-
+#arghhh
 class ArgumentsProfile(object):
     def __init__(self, function, *args, **kwargs):
         self.function = function
@@ -10,7 +10,9 @@ class ArgumentsProfile(object):
         self._raw_kwargs = kwargs
         
         args_spec = inspect.getargspec(function)
-        (s_args, s_star_args, s_star_kwargs, s_defaults) = args_spec
+        
+        (self._s_args, self._s_star_args,
+         self._s_star_kwargs, self._s_defaults) = args_spec
 
         for arg_name_or_list in s_args:
             self._process_arg_name_or_list(arg_name_or_list)
@@ -19,6 +21,7 @@ class ArgumentsProfile(object):
     def _process_arg_name_or_list(self, arg_name_or_list, raw_args, s_defaults):
         if isinstance(arg_name_or_list, basestring):
             arg_name = arg_name_or_list
+            
         else:
             assert isinstance(arg_name_or_list, list)
             
