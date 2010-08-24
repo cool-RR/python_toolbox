605880e80cc53366e507d1a8f45511c46aa615c4
Ram Rachum <cool-rr@cool-rr.com>
2010-08-24 03:18:01 +0200

    -

---
+++ garlicsim/garlicsim/general_misc/arguments_profile.py
@@ -0,0 +1,24 @@
+# Copyright 2009-2010 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+from garlicsim.general_misc.third_party import inspect
+
+class ArgumentsProfile(object):
+    def __init__(self, function, *args, **kwargs):
+        self.function = function
+        self._raw_args = args
+        self._raw_kwargs = kwargs
+        
+        args_spec = inspect.getargspec(function)
+        (s_args, s_star_args, s_star_kwargs, s_defaults) = args_spec
+
+        for arg_name_or_list in s_args:
+            self._process_arg_name_or_list(arg_name_or_list)
+                
+        
+    def _process_arg_name_or_list(self, arg_name_or_list, raw_args, s_defaults):
+        if isinstance(arg_name_or_list, basestring):
+            arg_name = arg_name_or_list
+        else:
+            assert isinstance(arg_name_or_list, list)
+            
