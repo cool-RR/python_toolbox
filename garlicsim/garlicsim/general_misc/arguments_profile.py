eca6d1d5b0e0ffc303025641260d2e1dd37d6caf
Ram Rachum <cool-rr@cool-rr.com>
2010-09-08 22:03:12 +0200

    -

--- garlicsim/garlicsim/general_misc/arguments_profile.py
+++ garlicsim/garlicsim/general_misc/arguments_profile.py
@@ -10,10 +10,16 @@ from garlicsim.general_misc import dict_tools
     #pass
 
 
-# Canonical: As few characters as possible, and after that as many keyword
-# arguments as possible.
+# Our grand definition of canonical: As few characters as possible, and after
+# that as many keyword arguments as possible, with extraneous keyword arguments
+# ordered alphabetically (with "_" as the highest character, so anything
+# starting with "_" will be at the end.)
 
 class ArgumentsProfile(object):
+    '''
+    Note this should be used only on functions that don't modify argmunets
+    '''
+    
     def __init__(self, function, *args, **kwargs):
         self.function = function
         raw_args = args
@@ -106,10 +112,73 @@ class ArgumentsProfile(object):
             # After we have the price for each option, we'll select the one with
             # the lowest price.
             
+            # One thing to do before iterating on the candidates is to find out
+            # whether the "lonely comma discount" is in effect.
+            # The "lonely comma discount" is given when there's nothing but
+            # defaultful arguments to this function, and therefore the number of
+            # ", " strings needed here is not `candidate`, but `candidate - 1`,
+            # unless of course candidate is zero.
+            
+            if not defaultless_args and \
+                (not s_star_args or not getcallargs_result[s_star_args]) and \
+                (not s_star_kwargs or not getcallargs_result[s_star_kwargs]):
+                
+                lonely_comma_discount_may_be_given = True
+            
+            else:
+                
+                lonely_comma_discount_may_be_given = False
+            
+            # Now we iterate on the candidates to find out which one has the
+            # lowest price:
+            
             for candidate in xrange(n_defaultful_args + 1):
-                price = 777 todo todo todo
-                # ...
+
+                defaultful_args_to_specify_positionally = \
+                    defaultful_args[:candidate]
+                
+                price_for_positionally_specified_defaultful_args = \
+                    2 * candidate + \
+                    sum(
+                        dict_tools.get_list(
+                            price_for_positionally_specified_defaultful_args,
+                            defaultful_args_to_specify_positionally
+                        )
+                    )
+                # The `2 * candidate` part is to account for the ", " parts
+                # between the arguments.
+                    
+                defaultful_args_to_specify_by_keyword = filter(
+                    values_differing_from_defaults.__contains__,
+                    defaultful_args[candidate:]
+                )
+                
+                price_for_defaultful_args_specified_by_keyword = \
+                    2 * candidate + \
+                    sum(
+                        dict_tools.get_list(
+                            price_for_defaultful_args_specified_by_keyword,
+                            defaultful_args_to_specify_by_keyword
+                        )
+                    )
+                # The `2 * candidate` part is to account for the ", " parts
+                # between the arguments.
+                
+                # Now we need to figure out if this candidate gets the "lonely
+                # comma discount".
+                if lonely_comma_discount_may_be_given and candidate > 0:
+                    lonely_comma_discount = -2
+                else:
+                    lonely_comma_discount = 0
+                
+                price = price_for_positionally_specified_defaultful_args + \
+                        price_for_defaultful_args_specified_by_keyword + \
+                        lonely_comma_discount
+                
                 total_price_for_n_dasp_candidate[candidate] = price
+
+
+            # Finished iterating on candidates! Time to pick our winner.
                 
             minimum_price = min(total_price_for_n_dasp_candidate.itervalues())
             
@@ -126,7 +195,40 @@ class ArgumentsProfile(object):
                 n_defaultful_args_to_specify_positionally = winner
             
             else:
-                # We have a draw! We're gonna have to settle it by
+                # We have a draw! We're gonna have to settle it by picking the
+                # lowest candidate, because in our definition of "canonical
+                # arguments profile", our second priority after "as few
+                # characters as possible" is "as many keyword arguments as
+                # possible".
+                winner = leading_candidates[0]
+                
+            n_defaultful_args_to_specify_positionally = winner
+            
+        # We have a winner! Now we know exactly which defaultful args should
+        # be specified positionally and which should be specified by
+        # keyword.
+            
+        # First we add the positionally specified:
+            
+        defaultful_args_to_specify_positionally = \
+            defaultful_args[:n_defaultful_args_to_specify_positionally]
+        self.args += [getcallargs_result[defaultful_arg] for defaultful_arg
+                      in defaultful_args_to_specify_positionally]
+        
+        # Now we add those specified by keyword:
+
+        defaultful_args_to_specify_by_keyword = filter(
+                values_differing_from_defaults.__contains__,
+                defaultful_args[n_defaultful_args_to_specify_positionally:]
+            )
+        for defaultful_arg in defaultful_args_to_specify_by_keyword:
+            self.kwargs[defaultful_arg] = getcallargs_result[defaultful_arg]
+                
+        #######################################################################
+        # Phase 3: ???
+            
+            
+            
                     
         
         
