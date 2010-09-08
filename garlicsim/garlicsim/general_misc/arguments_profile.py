e640f7e96c574987b4ef94f437982ed487b4feae
Ram Rachum <cool-rr@cool-rr.com>
2010-09-08 19:37:14 +0200

    -

--- garlicsim/garlicsim/general_misc/arguments_profile.py
+++ garlicsim/garlicsim/general_misc/arguments_profile.py
@@ -5,6 +5,9 @@ from garlicsim.general_misc.third_party import inspect
 from garlicsim.general_misc.third_party.ordered_dict import OrderedDict
 from garlicsim.general_misc import dict_tools
 
+class FinishedMission(Exception): # tododoc: Make inaccessible
+    pass
+
 # Canonical: As few characters as possible, and after that as many keyword
 # arguments as possible.
 
@@ -26,26 +29,46 @@ class ArgumentsProfile(object):
                                                  self._raw_args,
                                                  self._raw_kwargs)
         
+        # The number of args which have default values:
+        n_defaultful_args = len(s_defaults)
+        # The word "defaultful" means "something which has a default".
         
         #######################################################################
         # Phase 1: We specify all the args that don't have a default as
         # positional args:
-        defaultless_args = s_args[:-len(s_defaults)]
+        defaultless_args = s_args[:-n_defaultful_args]
         self.args += dict_tools.get_list(getcallargs_result, defaultless_args)
 
         
         #######################################################################
-        # Phase 2: ???
+        # Phase 2: We now have to deal with args that have a default. Some of
+        # them, possibly none and possibly all of them, should be given
+        # positionally. Some of them, possibly none, should be given by keyword.
+        # And some of them, possibly none and possibly all of them, should not be
+        # given at all. It is our job to figure out in which way each argument
+        # should be given.
+        
+        # In this variable:
+        n_defaultful_args_to_specify_positionally = None
+        # We will put the number of defaultful arguments that should be
+        # specified positionally.
         
         # Creating a dict that maps from argument name to default value:
-        args_with_defaults = OrderedDict(
-            zip(s_args[-len(s_defaults):], s_defaults)
+        defaultful_args = OrderedDict(
+            zip(s_args[-n_defaultful_args:], s_defaults)
         )
         
         args_differing_from_defaults = OrderedDict(
-            (key, value) for (key, value) in args_with_defaults.iteritems()
+            (key, value) for (key, value) in defaultful_args.iteritems()
             if value != getcallargs_result[key]
         )
+        
+        
+        
+        
+        
+        
+        
         
 
         for arg_name_or_list in s_args:
