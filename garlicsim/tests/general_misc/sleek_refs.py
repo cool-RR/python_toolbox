7b56d5687da93cf8843ea6fb35f6d655a361c842
cool-RR <cool-rr@cool-rr.com>
2010-09-12 22:36:37 +0200

    -

--- garlicsim/tests/general_misc/sleek_refs.py
+++ garlicsim/tests/general_misc/sleek_refs.py
@@ -27,9 +27,7 @@ def counter(_=None):
 
     
 def test_sleek_ref():
-    
-    
-            
+
     volatile_things = [A(), 1, 4.5, 'meow', u'woof', [1, 2], (1, 2), {1: 2},
                        set([1, 2, 3])]
     unvolatile_things = [A.s, __builtins__, list, type,  list.append, str.join,
@@ -63,7 +61,8 @@ def test_sleek_ref():
         assert counter() == count + 1
         assert sleek_ref() is not None
         
-def test_cute_sleek_value_dictionary_one_by_one():
+        
+def test_cute_sleek_value_dictionary():
     volatile_things = [A(), 1, 4.5, 'meow', u'woof', [1, 2], (1, 2), {1: 2},
                        set([1, 2, 3])]
     unvolatile_things = [A.s, __builtins__, list, type,  list.append, str.join,
@@ -72,9 +71,10 @@ def test_cute_sleek_value_dictionary_one_by_one():
     # Using len(csvd) as our key; Just to guarantee we're not running over an
     # existing key.
         
+    csvd = CuteSleekValueDictionary(counter)
+    
     while volatile_things:
         volatile_thing = volatile_things.pop()
-        csvd = CuteSleekValueDictionary(counter)
         if _is_weakreffable(volatile_thing):
             csvd[len(csvd)] = volatile_thing
             count = counter()
@@ -87,6 +87,7 @@ def test_cute_sleek_value_dictionary_one_by_one():
             del volatile_thing
             gc.collect()
             assert counter() == count + 1
+
             
     while unvolatile_things:
         unvolatile_thing = unvolatile_things.pop()
@@ -98,7 +99,8 @@ def test_cute_sleek_value_dictionary_one_by_one():
         gc.collect()
         assert counter() == count + 1
         
-def test_cute_sleek_value_dictionary():
+        
+def test_cute_sleek_value_dictionary_one_by_one():
     volatile_things = [A(), 1, 4.5, 'meow', u'woof', [1, 2], (1, 2), {1: 2},
                        set([1, 2, 3])]
     unvolatile_things = [A.s, __builtins__, list, type,  list.append, str.join,
