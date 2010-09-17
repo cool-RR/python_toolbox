de69dc6ac925c5a5c17189cc96b4a1d9ca1766f7
cool-RR <cool-rr@cool-rr.com>
2010-09-18 00:37:04 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/textual.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/textual.py
@@ -1,6 +1,5 @@
 import wx
 
-
 def ratio_to_round_degrees(hue):
     return int(ratio * 360)
 
@@ -16,18 +15,27 @@ class Textual(wx.Panel):
         
         self.main_v_sizer = wx.BoxSizer(wx.VERTICAL)
         
-        self.static_text = wx.StaticText(self, label='Hue:')
+        self.hue_static_text = wx.StaticText(self, label='Hue:')
+        #self.hue_static_text = wx.StaticText(self, label=''.join((chr(x) for x in range(256))))
         
-        self.main_v_sizer.Add(self.static_text, 0, wx.ALIGN_LEFT | wx.BOTTOM,
+        self.main_v_sizer.Add(self.hue_static_text, 0, wx.ALIGN_LEFT | wx.BOTTOM,
                               border=5)
         
         self.h_sizer = wx.BoxSizer(wx.HORIZONTAL)
         
         self.main_v_sizer.Add(self.h_sizer, 0)
         
-        self.spin_ctrl = wx.SpinCtrl(self, min=0, max=359)
+        self.spin_ctrl = wx.SpinCtrl(self, min=0, max=359, size=(60, -1))
         
-        '°'
+        self.h_sizer.Add(self.spin_ctrl, 0)
+        
+        #chr(248)
+        self.degree_static_text = wx.StaticText(self, label=unichr(176))
+        #self.degree_static_text.SetFont(
+            #wx.Font(12, wx.MODERN, wx.NORMAL, wx.NORMAL)
+        #)
+        
+        self.h_sizer.Add(self.degree_static_text, 0)
         
         self.SetSizerAndFit(self.main_v_sizer)
                     
