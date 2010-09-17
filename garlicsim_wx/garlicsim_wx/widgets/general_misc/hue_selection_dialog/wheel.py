10d1b90f47fa9f26c2918ac11e8e57c4d5ae7fbc
cool-RR <cool-rr@cool-rr.com>
2010-09-17 16:03:36 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/wheel.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/wheel.py
@@ -1,11 +1,17 @@
 import wx
 
 from garlicsim.general_misc import caching
+from garlicsim_wx.general_misc import wx_tools
+
+WIDTH, HEIGHT = 300, 300
 
 @caching.cache
 def make_bitmap(lightness=1, saturation=1):
     bitmap = wx.EmptyBitmap(300, 300)
-    dc = wx.MemoryDC(bitmap)    
+    dc = wx.MemoryDC(bitmap)
+    dc.SetBrush(wx_tools.get_background_brush())
+    dc.Dra
+    
     dc.SetPen(wx.Pen('red'))
     dc.DrawLine(0, 0, 100, 100)
     dc.Destroy()
