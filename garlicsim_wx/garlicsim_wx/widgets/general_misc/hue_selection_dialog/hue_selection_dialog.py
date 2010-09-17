ee1108f900b85b7b17f03e75918f75e6875d4b2c
cool-RR <cool-rr@cool-rr.com>
2010-09-18 01:25:50 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
@@ -38,12 +38,11 @@ class HueSelectionDialog(CuteDialog):
         
         self.wheel = Wheel(self)
         
-        self.h_sizer.Add(self.wheel, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL,
-                         border=10)
+        self.h_sizer.Add(self.wheel, 0, wx.ALL, border=10)
         
         self.v_sizer = wx.BoxSizer(wx.VERTICAL)
         
-        self.h_sizer.Add(self.v_sizer, 0)
+        self.h_sizer.Add(self.v_sizer, 0, wx.ALIGN_CENTER)
         
         self.comparer = Comparer(self)
         
