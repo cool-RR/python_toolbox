9fe8821d20f72afb6ed43b1fa61a2c0a008d3465
cool-RR <cool-rr@cool-rr.com>
2010-09-18 12:13:16 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/workspace_widgets/crunching_controls/step_profiles_controls/step_profiles_list.py
+++ garlicsim_wx/garlicsim_wx/widgets/workspace_widgets/crunching_controls/step_profiles_controls/step_profiles_list.py
@@ -61,30 +61,43 @@ class StepProfilesList(hypertreelist.HyperTreeList):
         self.gui_project.step_profiles_set_modified_emitter.add_output(
             self.update
         )
+        
+        self.gui_project.step_profiles_to_hues_modified_emitter.add_output(
+            self.update_colors
+        )
   
         
     def update(self):
 
         for step_profile in self.gui_project.step_profiles:
-            color = hue_to_dark_color(
-                self.gui_project.step_profiles_to_hues[step_profile]
-            )
             try:
                 item = self.step_profiles_to_items[step_profile]
             except KeyError:
-                #entry = StepProfileEntry(self, step_profile)
+                color = hue_to_dark_color(
+                    self.gui_project.step_profiles_to_hues[step_profile]
+                )
                 color_control = ColorControl(self, step_profile, color)
                 item = self.AppendItem(self.root_item, '', ct_type=2, wnd=color_control)
                 item.step_profile = step_profile
                 item.color_control = color_control
                 self.step_profiles_to_items[step_profile] = item
-            else:
-                item.color_control.set_color(color)
-        
-            self.SetItemText(item, step_profile.__repr__(short_form=True), 1)
+                self.SetItemText(item, step_profile.__repr__(short_form=True), 1)
+                
             item.color_control.SetSize((item.color_control.GetSize()[0],
                                        item.GetHeight() - 4))
         
         for item in self.items:
             if item.step_profile not in self.gui_project.step_profiles:
                 self.Delete(item)
+                item.color_control.Destroy()
+                
+                
+    def update_colors(self):
+        
+        for item in self.items:
+            color = hue_to_dark_color(
+                self.gui_project.step_profiles_to_hues[
+                    item.step_profile
+                ]
+            )
+            item.color_control.set_color(color)
