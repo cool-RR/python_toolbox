acb613677b5d41921dc7b187d31c6e565ff3be05
Ram Rachum <cool-rr@cool-rr.com>
2010-10-03 02:21:52 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/error_dialog.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/error_dialog.py
@@ -6,6 +6,6 @@ from garlicsim_wx.widgets.general_misc.cute_dialog import CuteDialog
 class ErrorDialog(wx.MessageDialog, CuteDialog):
     def __init__(self, parent, message, caption='Error'):
         wx.MessageDialog.__init__(self, parent, message, caption,
-                                  wx.CANCEL | wx.ICON_ERROR)
+                                  wx.OK | wx.ICON_ERROR)
         CuteDialog.__init__(self, skip_dialog_init=True)
         
