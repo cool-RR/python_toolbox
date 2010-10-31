3d1dc41c8d820f3a74d7b8a543ae7ba054f8578a
cool-RR <cool-rr@cool-rr.com>
2010-10-31 19:23:33 +0200

    -

--- garlicsim_wx/garlicsim_wx/misc/menu_bar/node_menu/fork_by_crunching_using_menu/fork_by_crunching_using_menu.py
+++ garlicsim_wx/garlicsim_wx/misc/menu_bar/node_menu/fork_by_crunching_using_menu/fork_by_crunching_using_menu.py
@@ -9,6 +9,7 @@ See its documentation for more info.
 
 from itertools import izip
 
+from garlicsim.general_misc.third_party.ordered_dict import OrderedDict
 import wx
 
 from garlicsim_wx.general_misc.cute_menu import CuteMenu
@@ -52,43 +53,52 @@ class ForkByCrunchingUsingMenu(CuteMenu):
         if not gui_project:
             return
         step_profiles = gui_project.step_profiles
+            
+        items = self._get_step_profile_items()
         
-        def insert_step_profile(step_profile, index):
-            step_profile_text = step_profile.__repr__(
+        def find_item_of_step_profile(step_profile):
+            matching_items = [item for item in items if
+                              item.step_profile == step_profile]
+            assert len(matching_items) in [0, 1]
+            if matching_items:
+                (matching_item,) = matching_items
+                return matching_item
+            else:
+                return None
+        
+        step_profiles_to_items = OrderedDict(
+            ((step_profile, find_item_of_step_profile(step_profile))
+             for step_profile in step_profiles)
+        )
+        
+        needed_items = filter(None, step_profiles_to_items.values())
+        unneeded_items = [item for item in items if (item not in needed_items)]
+        
+        for item in items:
+            self.RemoveItem(item)
+            
+        itemless_step_profiles = [
+            step_profile for step_profile in step_profiles if
+            (step_profiles_to_items[step_profile] is None)
+        ]
+        
+        for itemless_step_profile in itemless_step_profiles:
+            step_profile_text = itemless_step_profile.__repr__(
                 short_form=True,
                 root=gui_project.simpack,
                 namespace=gui_project.namespace
             )
-            new_item = self.Insert(
-                index,
+            new_item = wx.MenuItem(
+                None,
                 -1,
                 step_profile_text,
                 'Fork by crunching using %s' % step_profile_text
             )
             new_item.step_profile = step_profile
+            step_profiles_to_items[itemless_step_profile] = new_item
             
-        items = self._get_step_profile_items()
-        items_iterator = iter(items)
-        
-        step_profiles_enumerator = enumerate(step_profiles)
-
-        while True:
-            try:
-                current_item = items_iterator.next()
-            except StopIteration:
-                for i, step_profile in step_profiles_enumerator:
-                    insert_step_profile(step_profile, i)
-                break
-            else:
-                while True:
-                    i, step_profile = step_profiles_enumerator.next()                
-                    if current_item.step_profile == step_profile:
-                        break
-                    else:
-                        if current_item.step_profile not in step_profiles:
-                            self.RemoveItem(current_item)
-                        else:
-                            insert_step_profile(step_profile, i)
+        for i, item in enumerate(step_profiles_to_items.itervalues()):
+            self.InsertItem(i, item)
                 
         
         updated_items = self._get_step_profile_items()
