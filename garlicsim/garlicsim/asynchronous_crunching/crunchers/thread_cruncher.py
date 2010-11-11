571855059f5f31be5acce248a2c118ec107a29fe
Ram Rachum <cool-rr@cool-rr.com>
2010-11-11 11:38:27 +0200

    -

--- garlicsim/garlicsim/asynchronous_crunching/crunchers/thread_cruncher.py
+++ garlicsim/garlicsim/asynchronous_crunching/crunchers/thread_cruncher.py
@@ -1,3 +1,5 @@
+
+
 # Copyright 2009-2010 Ram Rachum.
 # This program is distributed under the LGPL2.1 license.
 
--- garlicsim_wx/garlicsim_wx/widgets/workspace_widgets/crunching_controls/cruncher_controls/cruncher_text_scrolled_panel.py
+++ garlicsim_wx/garlicsim_wx/widgets/workspace_widgets/crunching_controls/cruncher_controls/cruncher_text_scrolled_panel.py
@@ -10,6 +10,8 @@ class CruncherTextScrolledPanel(wx.lib.scrolledpanel.ScrolledPanel):
     def __init__(self, cruncher_selection_dialog):
         wx.lib.scrolledpanel.ScrolledPanel.__init__(self, cruncher_selection_dialog)
         self.SetupScrolling()
+        self.SetMinSize((self.MinSize[0], 300))
+        self.main_v_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.cruncher_text = wx.StaticText(
             self,
             label=string_tools.docstring_trim(
@@ -17,9 +19,11 @@ class CruncherTextScrolledPanel(wx.lib.scrolledpanel.ScrolledPanel):
                 __doc__
             )
         )
-        #self.general_text.SetSize((self.ClientSize[0] - 20, -1))
-        self.cruncher_text.Wrap(
-            self.ClientSize[0]# - self.cruncher_list_box.BestSize[0] - 20
-        )
-        #self.cruncher_text.SetSize(self.cruncher_text.GetEffectiveMinSize())
+        self.main_v_sizer.Add(self.cruncher_text, 1, wx.EXPAND)
         
+        #self.general_text.SetSize((self.ClientSize[0] - 20, -1))
+        #self.cruncher_text.Wrap(
+            #self.ClientSize[0]# - self.cruncher_list_box.BestSize[0] - 20
+        #)
+        #self.cruncher_text.SetSize(self.cruncher_text.GetEffectiveMinSize())
+        self.SetSizer(self.main_v_sizer)
