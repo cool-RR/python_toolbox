a860ee30cdc7e06c2a0f8a7310c887638924c4a3
Ram Rachum <cool-rr@cool-rr.com>
2010-11-24 15:02:08 +0200

    -

--- garlicsim/garlicsim/general_misc/monkeypatching_tools.py
+++ garlicsim/garlicsim/general_misc/monkeypatching_tools.py
@@ -3,7 +3,8 @@ import types
 def monkeypatch_method(class_, name=None):
     def decorator(function):
         name_ = name or function.__name__
-        new_method = types.MethodType(function, class_)
+        new_method = types.MethodType(function, None, class_)
+		# Was: new_method = types.MethodType(function, class_), is subtly wrong, make tests to prove
         setattr(class_, name_, new_method)
         return function
     return decorator
