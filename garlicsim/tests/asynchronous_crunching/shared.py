e218cd7204601e87828454fa079354617af7a12c
cool-RR <cool-rr@cool-rr.com>
2010-11-28 16:00:41 +0200

    -

---
+++ garlicsim/tests/asynchronous_crunching/shared.py
@@ -0,0 +1,4 @@
+from garlicsim.asynchronous_crunching.crunchers import ThreadCruncher
+
+class MustachedThreadCruncher(ThreadCruncher):
+    '''A trivial subclass of ThreadCruncher for use in testing.'''
