9ed691248491137e8b3be2eff4ada47a0a96386c
Ram Rachum <cool-rr@cool-rr.com>
2010-11-30 01:00:23 +0200

    -

--- garlicsim/garlicsim/misc/simpack_grokker/get_step_type.py
+++ garlicsim/garlicsim/misc/simpack_grokker/get_step_type.py
@@ -34,6 +34,7 @@ def _get_step_type(step_function):
         raise GarlicSimException("%s is not a callable object, so it can't be "
                                  "a step function." % step_function)
     name = step_function.__name__
+    
     if 'step' not in name:
         raise GarlicSimException(
             "%s is not a step function-- It doesn't have the word 'step' in "
@@ -41,16 +42,31 @@ def _get_step_type(step_function):
             "a `.step_type` attribute pointing to a step type. (Like "
             "`garlicsim.misc.simpack_grokker.step_types.SimpleStep`.)" \
             % step_function) #tododoc: test this
+    
     if 'inplace_step_generator' in name:
+        raise NotImplementedError('`inplace_step_generator` not yet '
+                                  'supported. It will probably become '
+                                  'available in GarlicSim 0.7 in mid-2011.')
         return InplaceStepGenerator
+    
     elif 'inplace_step' in name:
+        raise NotImplementedError('`inplace_step` not yet '
+                                  'supported. It will probably become '
+                                  'available in GarlicSim 0.7 in mid-2011.')
         return InplaceStep
+    
     elif 'history_step_generator' in name:
+        raise NotImplementedError('`history_step_generator` not yet. '
+                                  'supported. It will probably become '
+                                  'available in GarlicSim 0.7 in mid-2011.')
         return HistoryStepGenerator
+    
     elif 'step_generator' in name:
         return StepGenerator
+    
     elif 'history_step' in name:
         return HistoryStep
+    
     else:
         assert 'step' in name
         return SimpleStep
