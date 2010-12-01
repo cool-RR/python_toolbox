af5978a28bf7f86516e7ae5aed6adcd95e5340fc
Ram Rachum <cool-rr@cool-rr.com>
2010-12-01 14:39:25 -0800

    -

--- garlicsim/tests/prepare.sh
+++ garlicsim/tests/prepare.sh
@@ -1,1 +1,4 @@
+# This is here just for reference:
+
 export PYTHONPATH="/home/coolrr/Desktop/GarlicSim/garlicsim:/home/coolrr/Desktop/GarlicSim/garlicsim_lib:/home/coolrr/Desktop/GarlicSim/garlicsim_wx:"
+nosetests --exclude=unittest2 --with-coverage --cover-package=garlicsim,garlicsim_lib,garlicsim_wx
