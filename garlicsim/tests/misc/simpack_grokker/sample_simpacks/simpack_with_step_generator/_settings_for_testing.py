9301ffe8b22502ee9eed0272f971ff2273b13d34
Ram Rachum <cool-rr@cool-rr.com>
2010-12-01 21:59:56 +0200

    -

--- garlicsim/tests/misc/simpack_grokker/sample_simpacks/simpack_with_step_generator/_settings_for_testing.py
+++ garlicsim/tests/misc/simpack_grokker/sample_simpacks/simpack_with_step_generator/_settings_for_testing.py
@@ -3,7 +3,7 @@ import garlicsim
 from .state import State
 
 HISTORY_DEPENDENT = False
-N_STEP_FUNCTIONS = 1
-DEFAULT_STEP_FUNCTION = State.step
+N_STEP_FUNCTIONS = 2
+DEFAULT_STEP_FUNCTION = State.step_generator
 CRUNCHERS_LIST = [garlicsim.asynchronous_crunching.crunchers.ThreadCruncher,
                   garlicsim.asynchronous_crunching.crunchers.ProcessCruncher]
--- garlicsim/tests/misc/simpack_grokker/sample_simpacks/simpack_with_step_generator/state.py
+++ garlicsim/tests/misc/simpack_grokker/sample_simpacks/simpack_with_step_generator/state.py
@@ -8,6 +8,12 @@ class State(garlicsim.data_structures.State):
     
     def step(self):
         return State()
+    
+    def step_generator(self):
+        current_state = self
+        while True:
+            current_state = current_state.step()
+            yield current_state
         
     @staticmethod
     def create_root():
