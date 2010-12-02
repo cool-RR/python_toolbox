5f29f0181a076a76e40c82b94f9b69717cc088c5
cool-RR <cool-rr@cool-rr.com>
2010-12-02 22:22:47 +0200

    -

--- garlicsim/test_garlicsim/test_asynchronous_crunching/test_project/test_endable/test_endable.py
+++ garlicsim/test_garlicsim/test_asynchronous_crunching/test_project/test_endable/test_endable.py
@@ -81,7 +81,7 @@ def check(simpack, cruncher_type):
     
     state = simpack.State.create_root()
     
-    ### First, let's run for short periods so it doesn't end: #################
+    ### Running for short periods synchronically so it doesn't end: ###########
     #                                                                         #
     
     # Whether we run the simulation for one, two, three, or four iterations, the
@@ -110,9 +110,9 @@ def check(simpack, cruncher_type):
     assert len(iter_result_in_list) == len(result) == 5
     
     #                                                                         #
-    ### Done running for short periods so it doesn't end. #####################
+    ### Done running for short periods synchronically so it doesn't end. ######
     
-    ### Now, let's run it for longer periods to make it end: ##################
+    ### Now, let's run it for longer periods synchronically to make it end: ###
     #                                                                         #
     
     for i in [5, 6, 7]:
@@ -134,7 +134,7 @@ def check(simpack, cruncher_type):
     assert len(iter_result_in_list) == len(result) == 5
         
     #                                                                         #
-    ### Done running for longer periods to make it end. #######################
+    ### Done running for longer periods synchronically to make it end. ########
     
     
     
@@ -145,6 +145,9 @@ def check(simpack, cruncher_type):
     assert project.tree.lock._ReadWriteLock__writer is None
     
     root = project.root_this_state(state)
+    
+    ### Running for short periods asynchronically so it doesn't end: ##########
+    #                                                                         #
     
     project.begin_crunching(root, 4)
 
@@ -175,151 +178,144 @@ def check(simpack, cruncher_type):
     assert len(project.tree.roots) == 1
     
     assert len(project.tree.all_possible_paths()) == 2
-            
     
     node_3 = my_path.next_node(node_1)
     assert node_3.state.clock == 2
     
-    #project.begin_crunching(node_3, 3)
+    #                                                                         #
+    ### Done running for short periods asynchronically so it doesn't end. #####
     
-    #total_nodes_added = 0
-    #while project.crunching_manager.jobs:
-        #time.sleep(0.1)
-        #total_nodes_added += project.sync_crunchers()
-    #y = total_nodes_added
+    ### Now, let's run it for longer periods asynchronically to make it end: ##
+    #                                                                         #
+    
+    project.begin_crunching(node_3, 3)
+    
+    total_nodes_added = 0
+    while project.crunching_manager.jobs:
+        time.sleep(0.1)
+        total_nodes_added += project.sync_crunchers()
+    assert total_nodes_added == 3
         
-    #if y < 3:
-        ## For simpacks with long time intervals, we make sure at least 3 nodes
-        ## were created.
-        #path = node_3.make_containing_path()
-        #leaf = path[-1]
-        #project.simulate(
-            #leaf,
-            #math_tools.round_to_int(3 - y, up=True)
-        #)
-        #y += path.__len__(start=path.next_node(leaf))
+    assert len(project.tree.nodes) == 11
+    
+    paths = project.tree.all_possible_paths()
+    assert len(paths) == 3
+    
+    assert set(len(p) for p in paths) == set([5, x + 1, 3 + y])
     
     
-    #assert len(project.tree.nodes) == x + y + 4
-    
-    #paths = project.tree.all_possible_paths()
-    #assert len(paths) == 3
-    
-    #assert set(len(p) for p in paths) == set([5, x + 1, 3 + y])
-    
-    
-    #project.ensure_buffer(node_3, 3)
+    project.ensure_buffer(node_3, 3)
 
-    #total_nodes_added = 0
-    #while project.crunching_manager.jobs:
-        #time.sleep(0.1)
-        #total_nodes_added += project.sync_crunchers()
+    total_nodes_added = 0
+    while project.crunching_manager.jobs:
+        time.sleep(0.1)
+        total_nodes_added += project.sync_crunchers()
     
-    #assert len(project.tree.nodes) == x + y + 4 + total_nodes_added
-    #assert len(project.tree.all_possible_paths()) == 3
+    assert len(project.tree.nodes) == x + y + 4 + total_nodes_added
+    assert len(project.tree.all_possible_paths()) == 3
     
-    #assert project.tree.lock._ReadWriteLock__writer is None
+    assert project.tree.lock._ReadWriteLock__writer is None
     
-    #two_paths = node_3.all_possible_paths()
+    two_paths = node_3.all_possible_paths()
     
-    #assert len(two_paths) == 2
-    #(path_1, path_2) = two_paths
-    #get_clock_buffer = lambda path: (path[-1].state.clock - node_3.state.clock)
-    #(clock_buffer_1, clock_buffer_2) = [get_clock_buffer(p) for p in two_paths]
+    assert len(two_paths) == 2
+    (path_1, path_2) = two_paths
+    get_clock_buffer = lambda path: (path[-1].state.clock - node_3.state.clock)
+    (clock_buffer_1, clock_buffer_2) = [get_clock_buffer(p) for p in two_paths]
     
-    #project.ensure_buffer_on_path(node_3, path_1, get_clock_buffer(path_1) * 1.2)
+    project.ensure_buffer_on_path(node_3, path_1, get_clock_buffer(path_1) * 1.2)
     
-    #total_nodes_added = 0
-    #while project.crunching_manager.jobs:
-        #time.sleep(0.1)
-        #total_nodes_added += project.sync_crunchers()
+    total_nodes_added = 0
+    while project.crunching_manager.jobs:
+        time.sleep(0.1)
+        total_nodes_added += project.sync_crunchers()
     
-    #(old_clock_buffer_1, old_clock_buffer_2) = (clock_buffer_1, clock_buffer_2)
-    #(clock_buffer_1, clock_buffer_2) = [get_clock_buffer(p) for p in two_paths]
+    (old_clock_buffer_1, old_clock_buffer_2) = (clock_buffer_1, clock_buffer_2)
+    (clock_buffer_1, clock_buffer_2) = [get_clock_buffer(p) for p in two_paths]
     
-    #assert clock_buffer_1 / old_clock_buffer_1 >= 1.2 - FUZZ
-    #assert clock_buffer_2 == old_clock_buffer_2
+    assert clock_buffer_1 / old_clock_buffer_1 >= 1.2 - FUZZ
+    assert clock_buffer_2 == old_clock_buffer_2
     
-    #project.ensure_buffer_on_path(node_3, path_2, get_clock_buffer(path_2) * 1.3)
+    project.ensure_buffer_on_path(node_3, path_2, get_clock_buffer(path_2) * 1.3)
     
-    #total_nodes_added = 0
-    #while project.crunching_manager.jobs:
-        #time.sleep(0.1)
-        #total_nodes_added += project.sync_crunchers()
+    total_nodes_added = 0
+    while project.crunching_manager.jobs:
+        time.sleep(0.1)
+        total_nodes_added += project.sync_crunchers()
     
-    #(old_clock_buffer_1, old_clock_buffer_2) = (clock_buffer_1, clock_buffer_2)
-    #(clock_buffer_1, clock_buffer_2) = [get_clock_buffer(p) for p in two_paths]
+    (old_clock_buffer_1, old_clock_buffer_2) = (clock_buffer_1, clock_buffer_2)
+    (clock_buffer_1, clock_buffer_2) = [get_clock_buffer(p) for p in two_paths]
     
-    #assert clock_buffer_1 == old_clock_buffer_1
-    #assert clock_buffer_2 / old_clock_buffer_2 >= 1.3 - FUZZ
-    
-    
-    
-    #plain_root = project.create_root()
-    
-    #assert len(project.tree.roots) == 2
-    
-    #assert len(project.tree.all_possible_paths()) == 4
+    assert clock_buffer_1 == old_clock_buffer_1
+    assert clock_buffer_2 / old_clock_buffer_2 >= 1.3 - FUZZ
     
     
     
-    #assert project.tree.lock._ReadWriteLock__writer is None
+    plain_root = project.create_root()
     
-    #number_of_nodes = len(project.tree.nodes)
-    #iterator = project.iter_simulate(node_1, 10)
+    assert len(project.tree.roots) == 2
     
-    #assert project.tree.lock._ReadWriteLock__writer is None
+    assert len(project.tree.all_possible_paths()) == 4
     
-    #new_node = iterator.next()
-    #assert new_node is node_1
-    #assert len(project.tree.nodes) == number_of_nodes
     
-    #assert project.tree.lock._ReadWriteLock__writer is None
     
-    #new_node = iterator.next()
-    #assert new_node is not node_1
-    #assert new_node.parent is node_1
-    #assert len(project.tree.nodes) == number_of_nodes + 1
+    assert project.tree.lock._ReadWriteLock__writer is None
     
-    #bunch_of_new_nodes = tuple(iterator)
-    #for parent_node, kid_node in cute_iter_tools.consecutive_pairs(bunch_of_new_nodes):
-        #assert project.tree.lock._ReadWriteLock__writer is None
-        #assert isinstance(parent_node, garlicsim.data_structures.Node)
-        #assert isinstance(kid_node, garlicsim.data_structures.Node)
-        #assert parent_node.children == [kid_node]
-        #assert kid_node.parent is parent_node
+    number_of_nodes = len(project.tree.nodes)
+    iterator = project.iter_simulate(node_1, 10)
+    
+    assert project.tree.lock._ReadWriteLock__writer is None
+    
+    new_node = iterator.next()
+    assert new_node is node_1
+    assert len(project.tree.nodes) == number_of_nodes
+    
+    assert project.tree.lock._ReadWriteLock__writer is None
+    
+    new_node = iterator.next()
+    assert new_node is not node_1
+    assert new_node.parent is node_1
+    assert len(project.tree.nodes) == number_of_nodes + 1
+    
+    bunch_of_new_nodes = tuple(iterator)
+    for parent_node, kid_node in cute_iter_tools.consecutive_pairs(bunch_of_new_nodes):
+        assert project.tree.lock._ReadWriteLock__writer is None
+        assert isinstance(parent_node, garlicsim.data_structures.Node)
+        assert isinstance(kid_node, garlicsim.data_structures.Node)
+        assert parent_node.children == [kid_node]
+        assert kid_node.parent is parent_node
         
-    #assert len(project.tree.nodes) == number_of_nodes + 10
+    assert len(project.tree.nodes) == number_of_nodes + 10
     
-    #### Testing cruncher type switching: ######################################
+    ### Testing cruncher type switching: ######################################
     
-    #job_1 = project.begin_crunching(root, clock_buffer=Infinity)
-    #job_2 = project.begin_crunching(root, clock_buffer=Infinity)
+    job_1 = project.begin_crunching(root, clock_buffer=Infinity)
+    job_2 = project.begin_crunching(root, clock_buffer=Infinity)
     
-    #assert len(project.crunching_manager.crunchers) == 0
-    #assert project.sync_crunchers() == 0
-    #assert len(project.crunching_manager.crunchers) == 2
-    #(cruncher_1, cruncher_2) = project.crunching_manager.crunchers.values()
-    #assert type(cruncher_1) is cruncher_type
-    #assert type(cruncher_2) is cruncher_type
+    assert len(project.crunching_manager.crunchers) == 0
+    assert project.sync_crunchers() == 0
+    assert len(project.crunching_manager.crunchers) == 2
+    (cruncher_1, cruncher_2) = project.crunching_manager.crunchers.values()
+    assert type(cruncher_1) is cruncher_type
+    assert type(cruncher_2) is cruncher_type
     
-    #time.sleep(0.2) # Letting the crunchers start working
+    time.sleep(0.2) # Letting the crunchers start working
     
-    #project.crunching_manager.cruncher_type = MustachedThreadCruncher
-    #project.sync_crunchers()
-    #assert len(project.crunching_manager.crunchers) == 2
-    #(cruncher_1, cruncher_2) = project.crunching_manager.crunchers.values()
-    #assert type(cruncher_1) is MustachedThreadCruncher
-    #assert type(cruncher_2) is MustachedThreadCruncher
+    project.crunching_manager.cruncher_type = MustachedThreadCruncher
+    project.sync_crunchers()
+    assert len(project.crunching_manager.crunchers) == 2
+    (cruncher_1, cruncher_2) = project.crunching_manager.crunchers.values()
+    assert type(cruncher_1) is MustachedThreadCruncher
+    assert type(cruncher_2) is MustachedThreadCruncher
     
-    #project.crunching_manager.cruncher_type = cruncher_type
-    #project.sync_crunchers()
-    #assert len(project.crunching_manager.crunchers) == 2
-    #(cruncher_1, cruncher_2) = project.crunching_manager.crunchers.values()
-    #assert type(cruncher_1) is cruncher_type
-    #assert type(cruncher_2) is cruncher_type
+    project.crunching_manager.cruncher_type = cruncher_type
+    project.sync_crunchers()
+    assert len(project.crunching_manager.crunchers) == 2
+    (cruncher_1, cruncher_2) = project.crunching_manager.crunchers.values()
+    assert type(cruncher_1) is cruncher_type
+    assert type(cruncher_2) is cruncher_type
     
-    #### Finished testing cruncher type switching. #############################
+    ### Finished testing cruncher type switching. #############################
     
     
     
