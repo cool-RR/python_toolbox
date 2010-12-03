0b2a703af2c24216bdb5dccd9917b0ffdce69ed7
Ram Rachum <cool-rr@cool-rr.com>
2010-12-03 15:38:10 +0200

    -

--- garlicsim/test_garlicsim/test_asynchronous_crunching/test_project/sample_simpacks/history_dependent_simpack/_settings_for_testing.py
+++ garlicsim/test_garlicsim/test_asynchronous_crunching/test_project/sample_simpacks/history_dependent_simpack/_settings_for_testing.py
@@ -5,5 +5,7 @@ from .state import State
 ENDABLE = False
 HISTORY_DEPENDENT = True
 N_STEP_FUNCTIONS = 1
-DEFAULT_STEP_FUNCTION_TYPE = State.history_step
+DEFAULT_STEP_FUNCTION = State.history_step
+DEFAULT_STEP_FUNCTION_TYPE = \
+    garlicsim.misc.simpack_grokker.step_types.HistoryStep
 CRUNCHERS_LIST = [garlicsim.asynchronous_crunching.crunchers.ThreadCruncher]
