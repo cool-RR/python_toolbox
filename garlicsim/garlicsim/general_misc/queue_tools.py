e3602d1597365674ded0fb12edf58b3bbe689f9d
Ram Rachum <cool-rr@cool-rr.com>
2010-12-06 21:11:20 +0200

    -

--- garlicsim/garlicsim/general_misc/queue_tools.py
+++ garlicsim/garlicsim/general_misc/queue_tools.py
@@ -7,6 +7,11 @@ from __future__ import with_statement
 
 import Queue
 
+
+class _Sentinel(object):
+    pass
+
+
 def dump(queue):
     '''
     Empty all pending items in a queue and return them in a list.
@@ -17,19 +22,24 @@ def dump(queue):
 
 
 def iterate(queue, block=False, limit_to_original_size=False):
-    '''Iterate over the items in the queue.'''
+    '''
+    Iterate over the items in the queue.
+    
+    `limit_to_original_size` uses a sentinel, which can be a problem for queues
+    with a finite size which may get full, or if other code is `get`ting items
+    from the queue while we iterate.    
+    '''
     if limit_to_original_size:
-        for i in xrange(queue.qsize()):
-            try:
-                yield queue.get(block=block)
-            except Queue.Empty:
+        queue.put(_Sentinel)
+    while True:
+        try:
+            thing = queue.get(block=block)
+            if thing is _Sentinel:
+                assert limit_to_original_size
                 raise StopIteration
-    else: # not limit_to_original_size
-        while True:
-            try:
-                yield queue.get(block=block)
-            except Queue.Empty:
-                raise StopIteration
+            yield thing
+        except Queue.Empty:
+            raise StopIteration
 
 
 def get_item(queue, i):
@@ -42,6 +52,7 @@ def get_item(queue, i):
     with queue.mutex:
         return queue.queue[i]
 
+    
 def queue_as_list(queue):
     '''
     Get all the items in the queue as a list without removing them.
