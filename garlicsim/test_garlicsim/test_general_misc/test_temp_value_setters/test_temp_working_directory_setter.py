67cfc437a87fa1d90aefe60ac0dea535fe782231
Ram Rachum <cool-rr@cool-rr.com>
2010-12-06 23:59:04 +0200

    -

--- garlicsim/test_garlicsim/test_general_misc/test_temp_value_setters/test_temp_working_directory_setter.py
+++ garlicsim/test_garlicsim/test_general_misc/test_temp_value_setters/test_temp_working_directory_setter.py
@@ -12,27 +12,56 @@ def test():
     try:
         old_cwd = os.getcwd()
         with TempWorkingDirectorySetter(temp_dir):
-            assert os.getcwd() == temp_dir
-        assert os.getcwd() == old_cwd        
+            
+            # Note that on Mac OS, the working dir will be phrased differently,
+            # so we can't do `assert os.getcwd() == temp_dir`. Instead we'll
+            # create a small file and check we can access it:
+            
+            with open('just_a_file', 'w') as my_file:
+                my_file.write("One two three.")
+            
+            with open('just_a_file', 'r') as my_file:
+                assert my_file.read() == "One two three."
+        
+        with open(os.path.join(temp_dir, 'just_a_file'), 'r') as my_file:
+            assert my_file.read() == "One two three."
+        
+        assert os.getcwd() == old_cwd
     finally:
         shutil.rmtree(temp_dir)
     
     
 def test_exception():
     # Not using `assert_raises` here because getting the `with` suite in there
-    # will be tricky.
+    # would be tricky.
     temp_dir = tempfile.mkdtemp(prefix='temp_garlicsim_')
     try:
         old_cwd = os.getcwd()
         try:
             with TempWorkingDirectorySetter(temp_dir):
-                assert os.getcwd() == temp_dir
+                
+                # Note that on Mac OS, the working dir will be phrased
+                # differently, so we can't do `assert os.getcwd() == temp_dir`.
+                # Instead we'll create a small file and check we can access it:
+                
+                with open('just_a_file', 'w') as my_file:
+                    my_file.write("One two three.")
+                
+                with open('just_a_file', 'r') as my_file:
+                    assert my_file.read() == "One two three."
+                
                 raise MyException
+            
         except MyException:
-            assert os.getcwd() == old_cwd
+
+            with open(os.path.join(temp_dir, 'just_a_file'), 'r') as my_file:
+                assert my_file.read() == "One two three."
+                
         else:
             raise Exception
-        assert os.getcwd() == old_cwd
+        
+        with open(os.path.join(temp_dir, 'just_a_file'), 'r') as my_file:
+            assert my_file.read() == "One two three."
         
     finally:
         shutil.rmtree(temp_dir)
