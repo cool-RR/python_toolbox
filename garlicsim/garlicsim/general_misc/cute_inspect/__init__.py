5c970e5b8c6bef7778e37ad7db4c8c85e8468594
Ram Rachum <cool-rr@cool-rr.com>
2010-12-10 23:23:08 +0200

    -

---
+++ garlicsim/garlicsim/general_misc/cute_inspect/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2009-2010 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+This package defines a variety of general tools.
+
+These tools are completely decoupled from `garlicsim` and do not belong anywhere
+else.
+'''
