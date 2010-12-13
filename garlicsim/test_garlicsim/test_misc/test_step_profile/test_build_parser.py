f64c5321b0cf546665860d432f95956b0613bbcf
Ram Rachum <cool-rr@cool-rr.com>
2010-12-13 02:53:49 +0200

    -

--- garlicsim/test_garlicsim/test_misc/test_step_profile/test_build_parser.py
+++ garlicsim/test_garlicsim/test_misc/test_step_profile/test_build_parser.py
@@ -1,13 +1,51 @@
+import nose
+
 import garlicsim
+
 from garlicsim.misc import StepProfile
 
-from ..test_simpack_grokker.sample_simpacks import simpack
+from . import sample_simpack
 
 
 def test():
-    simpack_grokker = garlicsim.misc.SimpackGrokker(simpack)
-    parse_arguments_to_step_profile = StepProfile.build_parser(
-        simpack_grokker.default_step_function
-    )
+    simpack_grokker = garlicsim.misc.SimpackGrokker(sample_simpack)
     
-    parse_arguments_to_step_profile(
+    parse = \
+        StepProfile.build_parser(sample_simpack.State.step)
+    alternate_parse = \
+        StepProfile.build_parser(sample_simpack.State.alternate_step)
+    none_parse = \
+        StepProfile.build_parser(None)
+    
+    
+    default_step_profile = parse()
+    
+    step_profile = parse(1, 2, 3)
+    assert step_profile == default_step_profile == StepProfile(
+        sample_simpack.State.step,
+        1, 2, 3
+    )
+    assert step_profile == alternate_parse(step_profile) == none_parse(step_profile) == alternate_parse(step_profile=step_profile)
+    
+    
+    different_step_profile = parse(4, 5, 6)
+    assert different_step_profile == parse(a=4, c=6, b=5)
+    assert different_step_profile == parse(different_step_profile)
+    assert different_step_profile == parse(step_profile=different_step_profile)
+    
+    assert different_step_profile != step_profile
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
