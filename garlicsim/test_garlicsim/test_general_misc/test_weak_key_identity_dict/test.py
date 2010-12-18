f7219fc3cf074e89d5bf174da293fcd8beb4efdf
Ram Rachum <cool-rr@cool-rr.com>
2010-12-18 16:37:07 +0200

    -

--- garlicsim/test_garlicsim/test_general_misc/test_weak_key_identity_dict/test.py
+++ garlicsim/test_garlicsim/test_general_misc/test_weak_key_identity_dict/test.py
@@ -1,5 +1,28 @@
+import nose
 
 from garlicsim.general_misc.weak_key_identity_dict import WeakKeyIdentityDict
 
+
+class WeakreffableList(list):
+    pass
+
+
 def test():
-    assert False
+    wki_dict = WeakKeyIdentityDict()
+    my_weakreffable_list = WeakreffableList([1, 2])
+    wki_dict[my_weakreffable_list] = 7
+    assert my_weakreffable_list in wki_dict
+    assert wki_dict[my_weakreffable_list] == 7
+    identical_weakreffable_list = WeakreffableList([1, 2])
+    assert identical_weakreffable_list not in wki_dict
+    nose.tools.assert_raises(KeyError,
+                             lambda: wki_dict[identical_weakreffable_list])
+    
+    my_weakreffable_list.append(3)
+    assert my_weakreffable_list in wki_dict
+    assert wki_dict[my_weakreffable_list] == 7
+    
+    del wki_dict[my_weakreffable_list]
+    assert my_weakreffable_list not in wki_dict
+    nose.tools.assert_raises(KeyError,
+                             lambda: wki_dict[my_weakreffable_list])
