bfc04a424e93ff8cf8774fc65074e577e4be2493
Ram Rachum <cool-rr@cool-rr.com>
2010-12-20 22:52:32 +0200

    -

--- garlicsim/garlicsim/general_misc/pickle_tools.py
+++ garlicsim/garlicsim/general_misc/pickle_tools.py
@@ -1,20 +1,40 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Defines the `ReasonedBool` class.
+
+See its documentation for more details.
+'''
+
+
 import re
 import cPickle as pickle_module
 import pickle # Importing just to get dispatch table, not pickling with it.
 import copy_reg
 import types
 
-from garlicsim.general_misc import caching
 from garlicsim.general_misc import address_tools
 from garlicsim.general_misc import misc_tools
 
 
 def is_atomically_pickleable(thing):
+    '''
+    Return whether `thing` is an atomically pickleable object.
+    
+    "Atomically-pickleable" means that it's pickleable without considering any
+    other object that it contains or refers to. For example, a `list` is
+    atomically pickleable, even if it contains an unpickleable object, like a
+    `threading.Lock()`.
+    
+    However, the `threading.Lock()` itself is not atomically pickleable.
+    '''
     my_type = misc_tools.get_actual_type(thing)
     return _is_type_atomically_pickleable(my_type, thing)
 
 
 def _is_type_atomically_pickleable(type_, thing=None):
+    '''Return whether `type_` is an atomically pickleable type.'''
     try:
         return _is_type_atomically_pickleable.cache[type_]
     except KeyError:
@@ -26,11 +46,14 @@ def _is_type_atomically_pickleable(type_, thing=None):
     # Sub-function in order to do caching without crowding the main algorithm:
     def get_result():
         
+        # We allow a flag for types to painlessly declare whether they're
+        # atomically pickleable:
         if hasattr(type_, '_is_atomically_pickleable'):
             return type_._is_atomically_pickleable
         
         # Weird special case: `threading.Lock` objects don't have `__class__`.
         # We assume that objects that don't have `__class__` can't be pickled.
+        # (With the exception of old-style classes themselves.)
         if not hasattr(thing, '__class__') and \
            (not isinstance(thing, types.ClassType)):
             return False
@@ -38,7 +61,8 @@ def _is_type_atomically_pickleable(type_, thing=None):
         if not issubclass(type_, object):
             return True
         
-        def confirm_legit_pickling_exception(exception):
+        def assert_legit_pickling_exception(exception):
+            '''Assert that `exception` reports a problem in pickling.'''
             message = exception.args[0]
             segments = [
                 "can't pickle",
@@ -56,7 +80,7 @@ def _is_type_atomically_pickleable(type_, thing=None):
             try:
                 reduce_result = reduce_function(thing)
             except Exception, exception:
-                confirm_legit_pickling_exception(exception)
+                assert_legit_pickling_exception(exception)
                 return False
             else:
                 return True
@@ -64,9 +88,10 @@ def _is_type_atomically_pickleable(type_, thing=None):
         reduce_function = getattr(type_, '__reduce_ex__', None)
         if reduce_function:
             try:
-                reduce_result = reduce_function(thing, 0) # argument is protocol
+                reduce_result = reduce_function(thing, 0)
+                # (The `0` is the protocol argument.)
             except Exception, exception:
-                confirm_legit_pickling_exception(exception)
+                assert_legit_pickling_exception(exception)
                 return False
             else:
                 return True
@@ -76,7 +101,7 @@ def _is_type_atomically_pickleable(type_, thing=None):
             try:
                 reduce_result = reduce_function(thing)
             except Exception, exception:
-                confirm_legit_pickling_exception(exception)
+                assert_legit_pickling_exception(exception)
                 return False
             else:
                 return True
@@ -87,12 +112,11 @@ def _is_type_atomically_pickleable(type_, thing=None):
     _is_type_atomically_pickleable.cache[type_] = result
     return result
     
-    
-        
 _is_type_atomically_pickleable.cache = {}
  
 
-class FilteredObject(object): 
+class FilteredObject(object):
+    '''Placeholder for an object that was filtered out when pickling.'''
     def __init__(self, about): 
         self.about = about 
     def __repr__(self): 
@@ -106,13 +130,21 @@ _filtered_string_pattern = re.compile(
 )
  
 class CutePickler(object): 
-    '''Not subclassing because cPickle.Pickler doesn't support subclassing.'''
+    '''
+    Pickler which filters out non-pickleable objects.
+    
+    When the pickler comes upon a non-pickleable object it replaces it with a
+    marker which will cause it to become a `FilteredObject` upon unpickling.
+    
+    (Not subclassing `cPickle.Pickler` because it doesn't support subclassing.)
+    '''
     def __init__(self, file_, protocol=0): 
         pickler = self.pickler = pickle_module.Pickler(file_, protocol) 
         pickler.persistent_id = self.persistent_id
         self.dump, self.clear_memo = \
             pickler.dump, pickler.clear_memo
- 
+
+        
     def persistent_id(self, obj):
         if self.pre_filter:
             passed_pre_filter = self.pre_filter(obj)
@@ -126,12 +158,20 @@ class CutePickler(object):
                    address_tools.describe(obj)
         
     def pre_filter(self, thing):
+        '''Pre-filter `thing`, returning `False` if it shouldn't be pickled.'''
         return True
  
     
 class CuteUnpickler(object): 
     '''
-    Not subclassing because cPickle.Unpickler doesn't support subclassing.
+    Unpickler which replaces non-pickleable objects with `FilteredObject`s.
+    
+    When the corresponding `CutePickler` came upon non-pickleable objects it
+    replacef them with a markers which will cause `CuteUnpickler` to replace
+    them with `FilteredObject` instances upon unpickling.
+    
+    (Not subclassing `cPickle.Unpickler` because it doesn't support
+    subclassing.)
     '''
     def __init__(self, file_): 
         unpickler = self.unpickler = pickle_module.Unpickler(file_) 
@@ -141,8 +181,6 @@ class CuteUnpickler(object):
         # (Defaulting to `None` because `pickle.Unpickler` doesn't have
         # `noload`.)
 
-            
- 
     def persistent_load(self, id_string):
         match = _filtered_string_pattern.match(id_string)
         if match:
