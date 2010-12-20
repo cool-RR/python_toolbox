06f15cee63f87519880efcd96d5aa63a2437a142
Ram Rachum <cool-rr@cool-rr.com>
2010-12-20 23:55:00 +0200

    -

--- garlicsim/garlicsim/general_misc/temp_value_setters/temp_recursion_limit_setter.py
+++ garlicsim/garlicsim/general_misc/temp_value_setters/temp_recursion_limit_setter.py
@@ -1,13 +1,37 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Defines the `TempRecursionLimitSetter` class.
+
+See its documentation for more details.
+'''
+
 import sys
 
 from .temp_value_setter import TempValueSetter
 
 
 class TempRecursionLimitSetter(TempValueSetter):
-    def __init__(self, value):
-        assert isinstance(value, int)
+    '''
+    Context manager for temporarily setting a value to a variable.
+    
+    The value is set to the variable before the suite starts, and gets reset
+    back to the old value after the suite finishes.
+    '''
+    
+    def __init__(self, recursion_limit):
+        '''
+        Construct the `TempValueSetter`.
+        
+        `variable` may be either an (`object`, `attribute_string`) pair or a
+        `(getter, setter)` pair.
+        
+        `value` is the temporary value to set to the variable.
+        '''
+        assert isinstance(recursion_limit, int)
         TempValueSetter.__init__(
             self,
             (sys.getrecursionlimit, sys.setrecursionlimit),
-            value=value
+            value=recursion_limit
         )
