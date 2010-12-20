ba9758aee9e905a73da8dbbd260be03896705b3e
Ram Rachum <cool-rr@cool-rr.com>
2010-12-21 00:23:05 +0200

    -

--- garlicsim/garlicsim/general_misc/cute_profile/cute_profile.py
+++ garlicsim/garlicsim/general_misc/cute_profile/cute_profile.py
@@ -1,11 +1,51 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Defines the `profile_ready` decorator.
+
+See its documentation for more details.
+'''
+
 import functools
 
 from . import base_profile
 
 
-
 def profile_ready(condition=None, off_after=True, sort=2):
-    # todo: add condition option
+    '''
+    Decorator for setting a function to be ready for profiling.
+    
+    For example:
+    
+        @profile_ready()
+        def f(x, y):
+            do_something_long_and_complicated()
+            
+    The advantages of this over regular `cProfile` are:
+    
+     1. It doesn't interfere with the function's return value.
+     
+     2. You can set the function to be profiled *when* you want, on the fly.
+     
+    How can you set the function to be profiled? There are a few ways:
+    
+    You can set `f.profiling_on=True` for the function to be profiled on the
+    next call. It will only be profiled once, unless you set
+    `f.off_after=False`, and then it will be profiled every time until you set
+    `f.profiling_on=False`.
+    
+    You can also set `f.condition`. You set it to a condition function taking
+    as arguments the decorated function and any arguments (positional and
+    keyword) that were given to the decorated function. If the condition
+    function returns `True`, profiling will be on for this function call,
+    `f.condition` will be reset to `None` afterwards, and profiling will be
+    turned off afterwards as well. (Unless, again, `f.off_after` is set to
+    `False`.)
+    
+    `sort` is an `int` specifying which column the results will be sorted by.
+    '''
+    
     
     def decorator(function):
         
@@ -25,7 +65,8 @@ def profile_ready(condition=None, off_after=True, sort=2):
                     decorated.profiling_on = False
                     decorated.condition = None
                     
-                decorated.original_function # This line puts it in locals, weird
+                # This line puts it in locals, weird:
+                decorated.original_function
                 
                 base_profile.runctx(
                     'result = decorated.original_function(*args, **kwargs)',
@@ -45,3 +86,4 @@ def profile_ready(condition=None, off_after=True, sort=2):
         functools.update_wrapper(decorated, function)
         return decorated
     return decorator
+
