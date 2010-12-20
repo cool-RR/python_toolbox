7d8eefcacaa96a71a82fa0ea419d1b31b90237c6
Ram Rachum <cool-rr@cool-rr.com>
2010-12-20 23:15:38 +0200

    -

--- garlicsim/garlicsim/general_misc/sleek_refs/sleek_call_args.py
+++ garlicsim/garlicsim/general_misc/sleek_refs/sleek_call_args.py
@@ -19,13 +19,38 @@ __all__ = ['SleekCallArgs']
 
     
 class SleekCallArgs(object):
+    '''
+    A bunch of call args with a sleekref to them.
+    
+    "Call args" is a mapping of which function arguments get which values.
+    For example, for a function:
+    
+        def f(a, b=2):
+            pass
+            
+    The calls `f(1)`, `f(1, 2)` and `f(b=2, a=1)` all share the same call args.
+    
+    All the argument values are sleekreffed to avoid memory leaks. (See
+    documentation of `garlicsim.general_misc.sleek_refs.SleekRef` for more
+    details.
+    '''
     # What if we one of the args gets gc'ed before this SCA gets added to the
     # dictionary? It will render this SCA invalid, but we'll still be in the
-    # dict. So make note to user: Always keep reference to args and kwargs until
-    # the SCA gets added to the dict.
+    # dict. So make note to user: Always keep reference to args and kwargs
+    # until the SCA gets added to the dict.
     def __init__(self, containing_dict, function, *args, **kwargs):
+        '''
+        Construct the `SleekCallArgs`.
+        
+        `containing_dict` is the `dict` we'll try to remove ourselves from when
+        one of our sleekrefs dies. `function` is the function for which we
+        calculate call args from `*args` and `**kwargs`.
+        '''
         
         self.containing_dict = containing_dict
+        '''
+        `dict` we'll try to remove ourselves from when 1 of our sleekrefs dies.
+        '''
         
         args_spec = cute_inspect.getargspec(function)
         star_args_name, star_kwargs_name = \
@@ -35,6 +60,8 @@ class SleekCallArgs(object):
         del args, kwargs
         
         self.star_args_refs = []
+        '''Sleekrefs to star-args.'''
+        
         if star_args_name:
             star_args = call_args.pop(star_args_name, None)
             if star_args:
@@ -42,6 +69,7 @@ class SleekCallArgs(object):
                                        star_arg in star_args]
         
         self.star_kwargs_refs = {}
+        '''Sleerefs to star-kwargs.'''
         if star_kwargs_name:            
             star_kwargs = call_args.pop(star_kwargs_name, {})
             if star_kwargs:
@@ -49,6 +77,7 @@ class SleekCallArgs(object):
                                                            star_kwargs)
         
         self.args_refs = CuteSleekValueDict(self.destroy, call_args)
+        '''Mapping from argument name to value, sleek-style.'''
         
         # In the future the `.args`, `.star_args` and `.star_kwargs` attributes
         # may change, so we must record the hash now:
@@ -63,16 +92,20 @@ class SleekCallArgs(object):
         
     
     args = property(lambda self: dict(self.args_refs))
+    '''The arguments.'''
     
     star_args = property(
         lambda self:
             tuple((star_arg_ref() for star_arg_ref in self.star_args_refs))
     )
+    '''Extraneous arguments. (i.e. `*args`.)'''
     
     star_kwargs = property(lambda self: dict(self.star_kwargs_refs))
+    '''Extraneous keyword arguments. (i.e. `*kwargs`.)'''
     
         
     def destroy(self, _=None):
+        '''Delete ourselves from our containing `dict`.'''
         if self.containing_dict:
             try:
                 del self.containing_dict[self]
