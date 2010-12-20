64de89c4b77641adb4102dcb100c9a963ceea311
Ram Rachum <cool-rr@cool-rr.com>
2010-12-20 22:36:39 +0200

    -

--- garlicsim/garlicsim/general_misc/reasoned_bool.py
+++ garlicsim/garlicsim/general_misc/reasoned_bool.py
@@ -1,7 +1,30 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+Defines the `ReasonedBool` class.
+
+See its documentation for more details.
+'''
+
 
 class ReasonedBool(object):
+    '''
+    A variation on `bool` that also gives a `.reason`.
+    
+    This is useful when you want to say "This is False because... (reason.)"
+    
+    Unfortunately this class is not a subclass of `bool`, since Python doesn't
+    allow subclassing `bool`.    
+    '''
 
     def __init__(self, value, reason=None):
+        '''
+        Construct the `ReasonedBool`.
+        
+        `reason` is the reason *why* it has a value of `True` or `False`. It is
+        usually a string, but is allowed to be of any type.
+        '''
         self.value = bool(value)
         self.reason = reason
         
