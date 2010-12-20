0cf8c22aa2b2b7cb2daf47272218777b5cb0e54a
Ram Rachum <cool-rr@cool-rr.com>
2010-12-20 21:03:05 +0200

    -

--- garlicsim/garlicsim/general_misc/cmp_tools.py
+++ garlicsim/garlicsim/general_misc/cmp_tools.py
@@ -1,8 +1,20 @@
 # Copyright 2009-2011 Ram Rachum.
 # This program is distributed under the LGPL2.1 license.
 
+'''Defines various tools for comparisons.'''
+
 
 __all__ = ['underscore_hating_cmp']
+
+
+def underscore_hating_cmp(a, b):
+    '''Compare two strings, counting the "_" character as highest.'''
+    assert isinstance(a, basestring) and isinstance(b, basestring)
+    first_result = _leading_underscore_cmp(a, b)
+    if first_result != 0:
+        return first_result
+    return cmp(a, b)
+
 
 def _leading_underscore_count(string):
     '''Count the number of leading underscores that a string has.'''
@@ -14,6 +26,7 @@ def _leading_underscore_count(string):
             break
     return result
 
+
 def _leading_underscore_cmp(a, b):
     '''Compare which string has more leading underscores.'''
     return cmp(
@@ -21,10 +34,3 @@ def _leading_underscore_cmp(a, b):
         _leading_underscore_count(b)
     )
 
-def underscore_hating_cmp(a, b):
-    '''Compare which simpack has a "higher" name.'''
-    assert isinstance(a, basestring) and isinstance(b, basestring)
-    first_result = _leading_underscore_cmp(a, b)
-    if first_result != 0:
-        return first_result
-    return cmp(a, b)
