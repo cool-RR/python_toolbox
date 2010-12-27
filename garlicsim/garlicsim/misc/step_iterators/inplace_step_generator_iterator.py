454df814b778a92af85bc574fe73c55d427aef7d
Ram Rachum <cool-rr@cool-rr.com>
2010-12-28 00:33:51 +0200

    -

--- garlicsim/garlicsim/misc/step_iterators/inplace_step_generator_iterator.py
+++ garlicsim/garlicsim/misc/step_iterators/inplace_step_generator_iterator.py
@@ -8,7 +8,7 @@ import garlicsim
 from garlicsim.misc import BaseStepIterator, SimpackError, AutoClockGenerator
 
 
-class InplaceStepIterator(BaseStepIterator):
+class InplaceStepGeneratorIterator(BaseStepIterator):
     
     def __init__(self, state, step_profile):
         
@@ -16,8 +16,8 @@ class InplaceStepIterator(BaseStepIterator):
         '''
         The current state that will be crunched from on the next iteration.
         '''
-                
-        assert garlicsim.misc.simpack_grokker.step_types.InplaceStep.\
+        
+        assert garlicsim.misc.simpack_grokker.step_types.InplaceStepGenerator.\
                __instancecheck__(step_profile.step_function)
         
         self.step_function = step_profile.step_function
@@ -33,16 +33,37 @@ class InplaceStepIterator(BaseStepIterator):
         
         self.auto_clock_generator.make_clock(self.current_state)
         
+        self.__build_raw_generator()
+
+    
+    def __build_raw_generator(self):
+        '''Build a raw generator which will provide the states for us.'''
+        self.raw_generator = self.step_profile.step_function(
+            self.current_state,
+            *self.step_profile.args,
+            **self.step_profile.kwargs
+        )
         
+    
+    
     def next(self):
-        '''Crunch the next state.'''        
-        
-        return_value = self.step_function(self.current_state,
-                                          *self.step_profile.args,
-                                          **self.step_profile.kwargs)
-        assert return_value is None
-        
-        self._auto_clock(self.current_state)
+        '''Crunch the next state.'''
+        try:        
+            try:
+                yielded_value = self.raw_generator.next()
+            except StopIteration:
+                self.__build_raw_generator()
+                yielded_value = self.raw_generator.next()
+                
+            assert yielded_value is None
+                
+            self._auto_clock(self.current_state)
+            
+        except StopIteration:
+                raise SimpackError('The inplace step generator `%s` raised '
+                                   '`StopIteration` without yielding even '
+                                   'once.' % self.step_profile.step_function)
+                
         return self.current_state
                 
         
