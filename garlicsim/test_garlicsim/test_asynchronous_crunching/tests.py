03734ae000116eb966091c2b79a3332e898dfaae
Ram Rachum <cool-rr@cool-rr.com>
2011-01-11 08:31:25 -0500

    -

--- garlicsim/test_garlicsim/test_asynchronous_crunching/tests.py
+++ garlicsim/test_garlicsim/test_asynchronous_crunching/tests.py
@@ -1,6 +1,8 @@
 # Copyright 2009-2011 Ram Rachum.
 # This program is distributed under the LGPL2.1 license.
 
+'''Testing module for `garlicsim.asynchronous_crunching`.'''
+
 from __future__ import division
 
 import os
