08871239789ac3b341f89308a79fd58645495230
Ram Rachum <cool-rr@cool-rr.com>
2011-01-30 23:25:52 +0200

    -

--- garlicsim/test_garlicsim/__init__.py
+++ garlicsim/test_garlicsim/__init__.py
@@ -4,3 +4,33 @@
 '''Testing package for `garlicsim`.'''
 
 from .shared import verify_sample_simpack_settings
+
+def __bootstrap():
+    import os
+    import sys
+    from garlicsim.general_misc import import_tools    
+    if not import_tools.exists('garlicsim_lib'):
+        garlicsim_lib_candidate_path = os.path.realpath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                '..',
+                '..',
+                'garlicsim_lib'
+            )
+        )
+        sys.path.append(garlicsim_lib_candidate_path)
+        import garlicsim_lib
+    if not import_tools.exists('garlicsim_wx'):
+        garlicsim_wx_candidate_path = os.path.realpath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                '..',
+                '..',
+                'garlicsim_wx'
+            )
+        )
+        sys.path.append(garlicsim_wx_candidate_path)
+        import garlicsim_wx
+        
+    
+__bootstrap()
