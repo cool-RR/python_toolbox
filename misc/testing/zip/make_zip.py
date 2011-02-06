be1f864a10a8dde0669b89f7f6f0f75b77ee3b16
Ram Rachum <cool-rr@cool-rr.com>
2011-02-06 23:27:32 +0200

    -

--- misc/testing/zip/make_zip.py
+++ misc/testing/zip/make_zip.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Copyright 2009-2011 Ram Rachum.
 # This program is distributed under the LGPL2.1 license.
 
@@ -5,7 +6,12 @@
 '''
 
 import os.path
+import zipfile
 
-
+# tododoc: helpful error messages:
 assert __name__ == '__main__'
-assert os.p
+assert os.path.realpath('.') == \
+       os.path.realpath(os.path.join(os.getcwd(), 'misc', 'testing', 'zip'))
+
+# todo: define function for zipping a folder, then use it to make garlicsim,
+# garlicsim_lib and garlicsim_wx in build folder
