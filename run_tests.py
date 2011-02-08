f21c7834ace71fb986ea8e508091b118aec46ae7
Ram Rachum <cool-rr@cool-rr.com>
2011-02-08 11:47:21 -0500

    -

--- run_tests.py
+++ run_tests.py
@@ -5,8 +5,27 @@
 
 '''
 
+# blocktodo: make friendly error message if `nose` is missing
+# blocktodo: make friendly error message if launched from wrong path
+
 import sys
 import nose
 
+class TestProgram(nose.core.TestProgram):    
+    def makeConfig(self, env, plugins=None):
+        """Load a Config, pre-filled with user config files if any are
+        found.
+        """
+        cfg_files = ['garlicsim/setup.cfg',
+                     'garlicsim_lib/setup.cfg',
+                     'garlicsim_wx/setup.cfg']
+        if plugins:
+            manager = nose.core.PluginManager(plugins=plugins)
+        else:
+            manager = nose.core.DefaultPluginManager()
+        return nose.core.Config(
+            env=env, files=cfg_files, plugins=manager)
+
+    
 if __name__ == '__main__':
-    nose.main()
+    TestProgram()
