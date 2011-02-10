dfbfafefa130ccdf20979a9f099d6d7d111d1441
Ram Rachum <cool-rr@cool-rr.com>
2011-02-10 14:58:46 +0200

    -

---
+++ garlicsim/garlicsim/bootstrap/bootstrap_py2exe/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+
+'''
+
+import sys
+
+frozen = getattr(sys, 'frozen', None)
+
+if frozen:
+    if sys.version_info[:1] != (2, 6):
+        raise NotImplementedError('For Python versions other than 2.6 need to '
+                                  'bundle their respective `site.py` modules.')
+    assert 'site' not in sys.modules
+    from . import python26_site
+    
