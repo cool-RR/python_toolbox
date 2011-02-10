88ddf18e19779e04b2cdfd00294e0ea2dad8b06e
Ram Rachum <cool-rr@cool-rr.com>
2011-02-10 15:44:40 +0200

    -

--- garlicsim_wx/package_py2exe.py
+++ garlicsim_wx/package_py2exe.py
@@ -14,6 +14,12 @@ import sys
 assert os.path.realpath(os.getcwd()) == \
        os.path.realpath(os.path.split(__file__)[0])
 
+def assert_no_unknown_folders():
+    existing_folders = \
+        set([name for name in os.listdir('.') if os.path.isdir(name)])
+    assert existing_folders == \
+           set(('garlicsim_wx', 'test_garlicsim_wx', 'py2exe_cruft'))
+
 folders_to_delete = []
 for folder in ['build', 'garlicsim_wx.egg-info', 'py2exe_dist']:
     if os.path.isdir(folder):
@@ -25,7 +31,9 @@ if folders_to_delete:
         sys.stdout.write("Deleting the '%s' folder... " % folder_to_delete)
         shutil.rmtree(folder_to_delete)
         sys.stdout.write('Done.\n')
+    assert_no_unknown_folders()
 else: # No folders to delete
+    assert_no_unknown_folders()
     sys.stdout.write('No previous build folders to delete, all clean.\n')
 
 sys.stdout.write('Launching py2exe.\n')
