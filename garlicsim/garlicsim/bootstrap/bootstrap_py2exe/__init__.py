074710e741fbfd37182ba7190e1dc56381eb16df
Ram Rachum <cool-rr@cool-rr.com>
2011-02-10 15:02:56 +0200

    -

--- garlicsim/garlicsim/bootstrap/bootstrap_py2exe/__init__.py
+++ garlicsim/garlicsim/bootstrap/bootstrap_py2exe/__init__.py
@@ -10,7 +10,7 @@ import sys
 frozen = getattr(sys, 'frozen', None)
 
 if frozen:
-    if sys.version_info[:1] != (2, 6):
+    if sys.version_info[:2] != (2, 6):
         raise NotImplementedError('For Python versions other than 2.6 need to '
                                   'bundle their respective `site.py` modules.')
     assert 'site' not in sys.modules
