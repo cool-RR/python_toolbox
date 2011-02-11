fd3540c1519195e0467752fb60c80b867f61aba1
Ram Rachum <cool-rr@cool-rr.com>
2011-02-10 22:08:23 -0500

    -

--- run_tests.py
+++ run_tests.py
@@ -170,7 +170,7 @@ def loadTestsFromDir(self, path):
     examined.
     """
     from nose.loader import (log, add_path, op_abspath, op_isfile, op_isdir,
-                             Failure, remove_path)
+                             Failure, remove_path, sort_list)
     
     log.debug("load from dir %s", path)
     plugins = self.config.plugins
