c954c6e77b1076c526d5945b63dbdad1207f05b5
Ram Rachum <cool-rr@cool-rr.com>
2011-03-17 16:53:24 -0500

    -

--- garlicsim_lib/garlicsim_lib/simpacks/prisoner/__init__.py
+++ garlicsim_lib/garlicsim_lib/simpacks/prisoner/__init__.py
@@ -2,8 +2,7 @@
 # This program is distributed under the LGPL2.1 license.
 
 '''
-A simulation package for a repeating game of prisoner's dillema between a
-population of players with different strategies.
+Simpack for a repeating game of prisoner's dillema with natural selection.
 '''
 
 from .state import State
--- garlicsim_lib/garlicsim_lib/simpacks/prisoner/state.py
+++ garlicsim_lib/garlicsim_lib/simpacks/prisoner/state.py
@@ -2,11 +2,9 @@
 # This program is distributed under the LGPL2.1 license.
 
 '''
-A simulation package for a repeating game of prisoner's dillema between a
-population of players with different strategies.
+Simpack for a repeating game of prisoner's dillema with natural selection.
 '''
 
-import copy
 import random
 random.seed()
 
