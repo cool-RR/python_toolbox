22e351bd05c4e32931de8b78dec62d265870e0b7
Ram Rachum <cool-rr@cool-rr.com>
2011-03-19 00:55:56 -0500

    -

--- garlicsim_lib/garlicsim_lib/simpacks/prisoner/state.py
+++ garlicsim_lib/garlicsim_lib/simpacks/prisoner/state.py
@@ -8,6 +8,9 @@ Simpack for a repeating game of prisoner's dillema with natural selection.
 import random
 
 from garlicsim.general_misc import random_tools
+from garlicsim.general_misc.infinity import infinity
+
+
 import garlicsim.data_structures
 
 from .player_type import PlayerType
@@ -28,17 +31,31 @@ class State(garlicsim.data_structures.State):
         `n_rounds` is the number of rounds in a match.
         '''
         
+        assert -1 <= round <= (n_rounds - 1)
         self.round = round
-        '''The round number, with `-1` being the preparation pseudo-round.'''
+        '''
+        The round number.
         
+        `-1` is the preparation pseudo-round, `0` is the first round, and
+        `n_rounds - 1` is the last round in the match .
+        '''
         
+        assert 0 <= match <= infinity
         self.match = match
+        '''The match number, going from `0` to infinity.'''
+        
+        assert all(isinstance(player, BasePlayer) for player in players)
         self.players = players
+        '''The list of players that play against each other.'''
+        
+        assert n_rounds >= 1
         self.n_rounds = n_rounds
+        '''The number of rounds in a match.'''
         
     
     @staticmethod
     def create_root(n_players=70, n_rounds=7):
+        '''Create a plain and featureless world state.'''
         state = State(
             players=[player_types_list[i % len(player_types_list)]() for i
                      in xrange(n_players)],
@@ -50,6 +67,7 @@ class State(garlicsim.data_structures.State):
     
     @staticmethod
     def create_messy_root(n_players=70, n_rounds=7):
+        '''Create a random and messy world state.'''
         state = State(
             players=[PlayerType.create_random_strategy_player() for i
                      in xrange(n_players)],
@@ -60,6 +78,8 @@ class State(garlicsim.data_structures.State):
     
         
     def inplace_step(self):
+        '''Modify the state in-place to make it the next moment in time.'''
+        
         self.clock += 1
     
         self.round += 1
