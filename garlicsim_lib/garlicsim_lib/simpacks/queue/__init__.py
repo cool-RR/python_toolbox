9cde79e74eb61280f11ab2eba773afd3e097707b
Ram Rachum <cool-rr@cool-rr.com>
2011-03-21 01:51:45 +0200

    =

--- garlicsim_lib/garlicsim_lib/simpacks/queue/__init__.py
+++ garlicsim_lib/garlicsim_lib/simpacks/queue/__init__.py
@@ -4,12 +4,12 @@
 '''
 Simpack for simulating a queue.
 
-`Queueing Theory`_ is the mathematical study of waiting lines, or queues. These
-can be shoppers waiting in line for a cashier in a supermarket, or product
-components waiting in a complex queue to be processed by different machines in
-a factory. Queueing Theory has applications in diverse fields, including
-telecommunications, traffic engineering, computing and the design of factories,
-shops, offices and hospitals.
+`Queueing Theory`_ is the mathematical study of clients waiting in a queue.
+These can be shoppers waiting in line for a cashier in a supermarket, or
+product components waiting in a complex queue to be processed by different
+machines in a factory. Queueing Theory has applications in diverse fields,
+including telecommunications, traffic engineering, computing and the design of
+factories, shops, offices and hospitals.
 
 In this simple simpack, clients from an infinite population arrive at a
 facility and wait to be serviced by a bunch of servers. They wait in a single
