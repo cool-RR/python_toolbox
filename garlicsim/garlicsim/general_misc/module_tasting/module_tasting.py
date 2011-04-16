b70c6c0b08e173486f1d66a2ad9c9c5f489dbb4a
Ram Rachum <cool-rr@cool-rr.com>
2011-04-16 15:20:12 +0200

    -

---
+++ garlicsim/garlicsim/general_misc/module_tasting/module_tasting.py
@@ -0,0 +1,16 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+This module defines the `` class.
+
+See its documentation for more information.
+'''
+
+# blocktodo: Will need thread safety for when another thread is importing at
+# the same time.
+
+from garlicsim.general_misc.third_party import mock as mock_module
+
+def taste_module(path_or_address):
+    pass
