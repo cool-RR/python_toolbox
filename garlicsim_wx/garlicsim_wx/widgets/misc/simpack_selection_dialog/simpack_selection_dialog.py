01ed10c9e9492d775a4bc4fcec09d580e276d332
Ram Rachum <cool-rr@cool-rr.com>
2011-04-28 19:17:58 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_selection_dialog.py
+++ garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_selection_dialog.py
@@ -14,7 +14,7 @@ import pkgutil
 
 import wx
 
-from garlicsim.general_misc.cmp_tools import underscore_hating_cmp
+from garlicsim.general_misc.comparison_tools import underscore_hating_cmp
 from garlicsim.general_misc import address_tools
 from garlicsim.general_misc import path_tools
 from garlicsim.general_misc import import_tools
