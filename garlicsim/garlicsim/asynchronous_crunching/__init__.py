369d9b6846af55741a3a35d2c4d763ef0436ff43
Ram Rachum <cool-rr@cool-rr.com>
2011-04-28 19:12:38 +0200

    -

--- garlicsim/garlicsim/asynchronous_crunching/__init__.py
+++ garlicsim/garlicsim/asynchronous_crunching/__init__.py
@@ -10,7 +10,7 @@ The most important class defined here is `Project`, and it is the only class
 that the user needs to interact with. It employs all the other classes.
 '''
 
-from .obsolete_cruncher_error import ObsoleteCruncherError
+from .obsolete_cruncher_error import ObsoleteCruncherException
 from .history_browser import HistoryBrowser
 from .crunching_profile import CrunchingProfile
 from .base_cruncher import BaseCruncher
