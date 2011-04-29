bc064a09eb0520adb88b3fb8437715a405453798
Ram Rachum <cool-rr@cool-rr.com>
2011-04-29 20:31:38 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_selection_dialog.py
+++ garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_selection_dialog.py
@@ -172,9 +172,7 @@ class SimpackSelectionDialog(CuteDialog):
         refresh_id = wx.NewId()
         self.Bind(wx.EVT_MENU, self._on_refresh, id=refresh_id)
         self.add_accelerators(
-            {wx.WXK_F5: refresh_id,
-             wx_tools.back_keys: refresh_id,
-             wx_tools.forward_keys: refresh_id}
+            {wx.WXK_F5: refresh_id}
         )
         
         '''
