dd0348d5f7ffeb885163329fd127d08425adcffb
Ram Rachum <cool-rr@cool-rr.com>
2011-05-07 11:47:26 -0400

    -

--- garlicsim/garlicsim/general_misc/abc_tools.py
+++ garlicsim/garlicsim/general_misc/abc_tools.py
@@ -2,11 +2,14 @@
 # This program is distributed under the LGPL2.1 license.
 
 '''Defines tools related to abstract base classes from the `abc` module.'''
-# blocktodo: camelcase
+
 
 class AbstractStaticMethod(staticmethod):
     '''
     A combination of `abc.abstractmethod` and `staticmethod`.
+    
+    A method which (a) doesn't take a `self` argument and (b) must be
+    overridden in any subclass if you want that subclass to be instanciable.
     
     This class is good only for documentation; it doesn't enforce overriding
     methods to be static.
