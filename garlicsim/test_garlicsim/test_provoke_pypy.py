e2d90a4b8a0f75ea372564943979396474ecec83
Ram Rachum <cool-rr@cool-rr.com>
2011-05-09 15:20:15 +0200

    -

--- garlicsim/test_garlicsim/test_provoke_pypy.py
+++ garlicsim/test_garlicsim/test_provoke_pypy.py
@@ -1,19 +1,49 @@
-# blocktodo: delete this module
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''Testing module for tutorial-2.'''
+
+from __future__ import with_statement
 
 import os.path
 
-from garlicsim.general_misc import temp_file_tools
+from garlicsim.general_misc.temp_value_setters import \
+    TempWorkingDirectorySetter
 from garlicsim.general_misc import sys_tools
+from garlicsim.general_misc import import_tools
+from garlicsim.general_misc import temp_file_tools
 
+import garlicsim.scripts.start_simpack
 
-def test_provoking_pypy():
-    with temp_file_tools.TemporaryFolder() as temporary_folder:
-        with sys_tools.TempSysPathAdder(temporary_folder):
-            module_path = os.path.join(temporary_folder, 'module_meow.py')
-            with open(module_path, 'w') as module_file:
-                module_file.write('x = 5')
-            import module_meow
-            assert module_meow.x == 5
+#class Object(object):
+    #pass
+
+def test():
+    '''Test provoking Pypy.'''
+    # Asserting we don't have a `_coin_flip` on path already in some other
+    # place:
+    assert not import_tools.exists('_coin_flip')
+    
+    with temp_file_tools.TemporaryFolder(prefix='test_garlicsim_') \
+                                                          as temp_folder:
+        with TempWorkingDirectorySetter(temp_folder):
+            with sys_tools.OutputCapturer() as output_capturer:
+                garlicsim.scripts.start_simpack.start(
+                    argv=['start_simpack.py', '_coin_flip']
+                )
+            simpack_path = os.path.join(temp_folder, '_coin_flip')
+                                                                      
+            state_module_path = os.path.join(simpack_path, 'state.py')
+                         
+            with sys_tools.TempSysPathAdder(temp_folder):
+                import _coin_flip                
+
+                #o = Object()
+                #o.simpack = _coin_flip
+                simpack_grokker = garlicsim.misc.SimpackGrokker(_coin_flip)
+                
+                
             
-
-    assert module_meow.x == 5
+            
+    
+        
