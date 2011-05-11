3758ce68467d4651df05605670fdb27654874065
Ram Rachum <cool-rr@cool-rr.com>
2011-05-10 23:40:57 -0400

    -

--- garlicsim/test_garlicsim/test_general_misc/test_address_tools/test_describe.py
+++ garlicsim/test_garlicsim/test_general_misc/test_address_tools/test_describe.py
@@ -276,7 +276,10 @@ def test_function_in_main():
     with TempValueSetter((globals(), '__name__'), '__main__'):
         def f(x):
             pass
+        
+        # Accessing `f.__module__` here so PyPy will calculate it:
         assert f.__module__ == '__main__'
+        
     assert f.__module__ == '__main__'
     import __main__
     __main__.f = f
