ea9b86be95f10102a7a7d4eab6a0d957dd53e68e
Ram Rachum <cool-rr@cool-rr.com>
2011-05-25 19:53:09 -0400

    -

--- garlicsim/garlicsim/general_misc/freezers/__init__.py
+++ garlicsim/garlicsim/general_misc/freezers/__init__.py
@@ -2,6 +2,9 @@
 # This program is distributed under the LGPL2.1 license.
 
 '''
+Defines the `Freezer` and `FreezerProperty` classes.
+
+See their documentation for more information.
 '''
 
 from .freezer import Freezer
