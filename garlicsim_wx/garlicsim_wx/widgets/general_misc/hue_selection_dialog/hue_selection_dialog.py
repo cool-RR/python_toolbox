d07c8821db7760bbf822c7cea99a241c20757336
Ram Rachum <cool-rr@cool-rr.com>
2011-06-01 14:59:33 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/hue_selection_dialog/hue_selection_dialog.py
@@ -29,52 +29,62 @@ class HueSelectionDialog(CuteDialog):
         
         CuteDialog.__init__(self, parent, id, title, pos, size, style, name)
         
+        ### Defining attributes: ##############################################
+        #                                                                     #
         self.getter = getter
+        '''Getter function for getting the current hue.'''
         
         self.setter = setter
+        '''Setter function for setting a new hue.'''
         
         assert isinstance(emitter, Emitter)
         self.emitter = emitter
+        '''Optional emitter to emit to when changing hue. May be `None`.'''
         
         self.lightness = lightness
+        '''The constant lightness of the colors that we're displaying.'''
 
         self.saturation = saturation
+        '''The constant saturation of the colors that we're displaying.'''
         
         self.hue = getter()
+        '''The current hue.'''
         
         self.old_hue = self.hue
+        '''The hue as it was before changing, when the dialog was created.'''
         
         self.old_hls = (self.old_hue, lightness, saturation)
+        '''
+        The hls color as it was before changing, when the dialog was created.
+        '''
+        #                                                                     #
+        ### Finished defining attributes. #####################################
         
+        self.__init_build()
         
+        self.emitter.add_output(self.update)
         
-        
+
+    def __init_build(self):
+        '''Build the widget.'''
         self.main_v_sizer = wx.BoxSizer(wx.VERTICAL)
-        
         self.h_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        
         self.main_v_sizer.Add(self.h_sizer, 0)
         
         self.wheel = Wheel(self)
-        
         self.h_sizer.Add(self.wheel, 0)
         
-        self.v_sizer = wx.BoxSizer(wx.VERTICAL)
-        
+        self.v_sizer = wx.BoxSizer(wx.VERTICAL)        
         self.h_sizer.Add(self.v_sizer, 0, wx.ALIGN_CENTER)
-        
         self.comparer = Comparer(self)
-        
         self.v_sizer.Add(self.comparer, 0, wx.RIGHT | wx.TOP | wx.BOTTOM,
                          border=10)
         
         self.textual = Textual(self)
-        
         self.v_sizer.Add(self.textual, 0, wx.RIGHT | wx.TOP | wx.BOTTOM,
                          border=10)
                 
         self.dialog_button_sizer = wx.StdDialogButtonSizer()
-        
         self.main_v_sizer.Add(self.dialog_button_sizer, 0,
                               wx.ALIGN_CENTER | wx.ALL, border=10)
         
@@ -89,26 +99,9 @@ class HueSelectionDialog(CuteDialog):
         self.Bind(wx.EVT_BUTTON, self._on_cancel, source=self.cancel_button)
         self.dialog_button_sizer.Realize()
 
-        
         self.SetSizer(self.main_v_sizer)
         self.main_v_sizer.Fit(self)
         
-        
-        self.emitter.add_output(self.update)
-
-
-    def ShowModal(self):
-        wx.CallAfter(self.textual.set_focus_on_spin_ctrl_and_select_all)
-        return super(HueSelectionDialog, self).ShowModal()
-        
-        
-    def _on_ok(self, event):
-        self.EndModal(wx.ID_OK)
-        
-    
-    def _on_cancel(self, event):
-        self.setter(self.old_hue)
-        self.EndModal(wx.ID_CANCEL)
         
         
     def update(self):
@@ -117,11 +110,35 @@ class HueSelectionDialog(CuteDialog):
         self.wheel.update()
         self.comparer.update()
         self.textual.update()
+
+
+    ### Overriding `wx.Dialog` methods: #######################################
+    #                                                                         #
+    def ShowModal(self):
+        '''Show the dialog modally. Overridden to focus on `self.textual`.'''
+        wx.CallAfter(self.textual.set_focus_on_spin_ctrl_and_select_all)
+        return super(HueSelectionDialog, self).ShowModal()
         
         
     def Destroy(self):
         self.emitter.remove_output(self.update)
         super(HueSelectionDialog, self).Destroy()
+    #                                                                         #
+    ### Finished overriding `wx.Dialog` methods. ##############################
+            
+    ### Event handlers: #######################################################
+    #                                                                         #
+    def _on_ok(self, event):
+        '''Handler for "Ok" button.'''
+        self.EndModal(wx.ID_OK)
+        
+    
+    def _on_cancel(self, event):
+        '''Handler for "Cancel" button.'''
+        self.setter(self.old_hue)
+        self.EndModal(wx.ID_CANCEL)
+    #                                                                         #
+    ### Finished event handlers. ##############################################
 
 
 from .comparer import Comparer
