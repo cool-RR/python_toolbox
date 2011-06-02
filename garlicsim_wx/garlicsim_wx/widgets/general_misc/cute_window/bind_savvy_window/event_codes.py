42f9385b53b839462dbf64723242b90d0644a7d3
Ram Rachum <cool-rr@cool-rr.com>
2011-06-02 14:15:32 -0400

    -

---
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/cute_window/bind_savvy_window/event_codes.py
@@ -0,0 +1,33 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+This module defines the `` class.
+
+See its documentation for more information.
+'''
+
+import wx
+
+from garlicsim.general_misc import caching
+
+
+map = {
+    wx.Button: wx.EVT_BUTTON,
+    wx.Menu: wx.EVT_MENU,
+}
+
+def get_event_code_of_component(component):
+    if hasattr(component, '_EventHandlerGrokker__event_code'):
+        return component._EventHandlerGrokker__event_code
+    else:
+        return _get_event_code_of_component_type(type(component))
+    
+@caching.cache()
+def _get_event_code_of_component_type(component_type):    
+    if hasattr(component_type, '_EventHandlerGrokker__event_code'):
+        return component_type._EventHandlerGrokker__event_code
+    else:
+        return map[component_type]
+           
+    
--- garlicsim_wx/garlicsim_wx/widgets/general_misc/cute_window/bind_savvy_window/event_handler_grokker.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/cute_window/bind_savvy_window/event_handler_grokker.py
@@ -9,10 +9,36 @@ See its documentation for more information.
 
 import wx
 
+from garlicsim.general_misc import caching
+
+from .event_codes import get_event_code_of_component
+
 
 class EventHandlerGrokker(object):
-    @classmethod
-    def create_from_name(cls, name, event_handler):
+    def __init__(self, name, event_handler):
         assert name.startswith('_on_')
+        self.name = name
+        self.event_handler = event_handler
+        
+    cleaned_name = caching.CachedProperty(
+        lambda self: self.name[4:],
+        doc=''' '''
+    )
+        
+    def bind(self, window):
+        assert isinstance(window, wx.Window)
+        if hasattr(window, self.cleaned_name):
+            component = getattr(window, self.cleaned_name)
+            event_code = get_event_code_of_component(component)
+            window.Bind(
+                event_code,
+                self.event_handler,
+                source=component
+            )
+        else:
+            
+                
+            
+        
         
         
