440950fa568e2759821881d5a0a1d4934ebf4180
Ram Rachum <cool-rr@cool-rr.com>
2011-06-17 19:15:43 -0400

    -

--- garlicsim_wx/garlicsim_wx/widgets/general_misc/cute_window/bind_savvy_window/event_handler_grokker.py
+++ garlicsim_wx/garlicsim_wx/widgets/general_misc/cute_window/bind_savvy_window/event_handler_grokker.py
@@ -17,22 +17,36 @@ from .event_codes import get_event_code_of_component, get_event_code_from_name
 
 
 class EventHandlerGrokker(object):
-    def __init__(self, name, event_handler_unbound_method, window_type):
+    '''Wraps an event handling function and figures out what to bind it to.'''
+    
+    def __init__(self, name, event_handler_self_taking_function, window_type):
+        '''
+        Construct the `EventHandlerGrokker`.
+        
+        `name` is the name of the event handling function.
+        `event_handler_self_taking_function` is the function itself, as proper
+        function. (i.e. taking two arguments `self` and `event`.) `window_type`
+        is the class in which that event handler is defined.
+        '''
         assert name.startswith('_on_')
+        
         self.name = name
-        self.event_handler_unbound_method = event_handler_unbound_method
-        # blocktodo: rename? not really method?
+        
+        self.event_handler_self_taking_function = event_handler_self_taking_function
+        
         self.window_type = window_type
+        
         
     cleaned_name = caching.CachedProperty(
         lambda self: self.name[4:],
         doc=''' '''
     )
-        
+
+    
     def bind(self, window):
         assert isinstance(window, wx.Window)
         event_handler_bound_method = types.MethodType(    
-            self.event_handler_unbound_method,
+            self.event_handler_self_taking_function,
             window,
             self.window_type
         )
