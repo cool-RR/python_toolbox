05e966f9f4ed44a6bfe039053473931a77ac8d4c
Ram Rachum <cool-rr@cool-rr.com>
2011-06-24 00:41:10 +0200

    -

--- garlicsim/test_garlicsim/test_general_misc/test_sequence_tools/test_to_tuple.py
+++ garlicsim/test_garlicsim/test_general_misc/test_sequence_tools/test_to_tuple.py
@@ -3,4 +3,67 @@
 
 '''Testing module for `sequence_tools.to_tuple`.'''
 
+from __future__ import with_statement
 
+from garlicsim.general_misc import cute_testing
+
+from garlicsim.general_misc import sequence_tools
+from garlicsim.general_misc.sequence_tools import to_tuple
+
+
+def test():
+    assert to_tuple((1, 2, 3)) == (1, 2, 3)
+    assert to_tuple([1, 2, 3]) == (1, 2, 3)
+    assert to_tuple(7) == (7,)
+    assert to_tuple((7,)) == (7,)
+    assert to_tuple(Ellipsis) == (Ellipsis,)
+    
+def test_item_type():
+    assert to_tuple(7, item_type=int) == (7,)
+    assert to_tuple([7], item_type=list) == ([7],)
+    assert to_tuple((7,), item_type=tuple) == ((7),)
+    
+    
+def test_item_test():
+    
+    def is_int_like(item):
+        '''Is `item` something like an `int`?'''
+        try:
+            1 + item
+        except Exception:
+            return False
+        else:
+            return True
+    
+    def is_list_like(item):
+        '''Is `item` something like an `int`?''' WAS HERE
+        try:
+            1 + item
+        except Exception:
+            return False
+        else:
+            return True
+    
+    def is_int_like(item):
+        '''Is `item` something like an `int`?'''
+        try:
+            1 + item
+        except Exception:
+            return False
+        else:
+            return True
+    
+    assert to_tuple(7, item_test=is_int_like) == (7,)
+    assert to_tuple([7], item_type=list) == ([7],)
+    assert to_tuple((7,), item_type=tuple) == ((7),)
+    
+    
+def test_too_many_arguments():
+    
+    with cute_testing.RaiseAssertor(text='either'):
+        to_tuple(
+            (1, 2, 3),
+            item_type=int,
+            item_test=lambda item: isinstance(item, int)
+        )
+        
