147a990ed39ffaf91c7601b39ba8524f31408701
Ram Rachum <cool-rr@cool-rr.com>
2011-07-08 12:39:39 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/filter_box.py
+++ garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/filter_box.py
@@ -10,6 +10,7 @@ import wx
 
 from garlicsim_wx.general_misc import emitters
 from garlicsim_wx.widgets.general_misc.cute_control import CuteControl
+from garlicsim_wx.general_misc import wx_tools
 
 
 filter_help_text = ('Type text in the filter box in order to filter the '
@@ -20,21 +21,35 @@ filter_help_text = ('Type text in the filter box in order to filter the '
 
 class FilterBox(wx.SearchCtrl, CuteControl):
     def __init__(self, navigation_panel):
-        wx.SearchCtrl.__init__(self, navigation_panel)
+        wx.SearchCtrl.__init__(
+            self,
+            navigation_panel,
+        )
         self.navigation_panel = navigation_panel
         self.ShowCancelButton(True)
         self.SetDescriptiveText('')
         self.SetHelpText(filter_help_text)
-        self.bind_event_handlers(FilterBox)
         self.filter_words = ''
         self.filter_words_changed_emitter = emitters.Emitter(
                 name='filter_words_changed',
         )
-    
-        
+        self.bind_event_handlers(FilterBox)
+
+            
     def _on_text(self, event):
-        event.Skip()
         new_filter_words = self.Value.split()
         if new_filter_words != self.filter_words:
             self.filter_words = new_filter_words
             self.filter_words_changed_emitter.emit()
+        
+            
+    def _on_text_enter(self, event):
+        # Currently not used because `TE_PROCESS_ENTER` also causes Tab to stop
+        # working for navigation on Windows. I couldn't solve this.
+        self.navigation_panel.simpack_selection_dialog.simpack_tree.SetFocus()
+        
+    
+    def _on_searchctrl_cancel_btn(self, event):
+        self.Value = ''
+        self.filter_words_changed_emitter.emit()
+    
