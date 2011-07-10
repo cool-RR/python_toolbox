7cbc3b4368294397a8664dd4183e3dbb26873e07
Ram Rachum <cool-rr@cool-rr.com>
2011-07-09 20:25:15 -0400

    -

--- garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_info_panel/tags_display.py
+++ garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_info_panel/tags_display.py
@@ -19,12 +19,26 @@ class TagsDisplay(CutePanel):
     def __init__(self, simpack_info_panel):
         self.simpack_info_panel = simpack_info_panel
         CutePanel.__init__(self, simpack_info_panel)
+        self.Hide()
+        
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.SetSizer(self.sizer)
+                
+        self.tags_static_text = wx.StaticText(self, label='&Tags:')
+        self.sizer.Add(self.tags_static_text,
+                       proportion=0,
+                       flag=(wx.ALIGN_TOP | wx.RIGHT),
+                       border=2)
+        
+
+        
         
     def refresh(self):
         simpack_metadata = \
             self.simpack_info_panel.simpack_selection_dialog.simpack_metadata
         if simpack_metadata is not None:
             self.Show()
+            self.Layout()
         else: # simpack_metadata is None
             self.Hide()
             
