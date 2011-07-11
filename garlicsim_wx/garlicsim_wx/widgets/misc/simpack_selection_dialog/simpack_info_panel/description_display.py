78bd15bcf5a23174ccce1c4388f4bed89a1363ce
Ram Rachum <cool-rr@cool-rr.com>
2011-07-11 22:10:54 +0200

    -

--- garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_info_panel/description_display.py
+++ garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_info_panel/description_display.py
@@ -97,7 +97,7 @@ class DescriptionDisplay(CuteHtmlWindow):
         CuteHtmlWindow.__init__(
             self,
             simpack_info_panel,
-            style=(wx.html.HW_DEFAULT_STYLE | wx.SUNKEN_BORDER)
+            style=(wx.html.HW_DEFAULT_STYLE | wx.SIMPLE_BORDER)
         )
         self.Hide()
         
