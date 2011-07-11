47cf902c622a06ac533673eeda94732a0679dc01
Ram Rachum <cool-rr@cool-rr.com>
2011-07-11 19:58:48 +0200

    -

--- garlicsim/garlicsim/general_misc/module_tasting/tasted_resources.py
+++ garlicsim/garlicsim/general_misc/module_tasting/tasted_resources.py
@@ -7,6 +7,7 @@ This module defines the `` class.
 See its documentation for more information.
 '''
 
+import types
 import sys
 
 import pkg_resources
@@ -15,15 +16,19 @@ import pkg_resources
 from .module_tasting import taste_module
 
 
-def get_provider(module_name_or_req):
-    """Get an `IResourceProvider` for the named module or requirement"""
-    if isinstance(module_name_or_req, pkg_resources.Requirement):
-        return pkg_resources.working_set.find(module_name_or_req) or \
-                              pkg_resources.require(str(module_name_or_req))[0]
-    try:
-        tasted_module = sys.modules[module_name_or_req]
-    except KeyError:
-        tasted_module = taste_module(module_name_or_req)
+def get_provider(module_or_name_or_req):
+    '''Get an `IResourceProvider` for the named module or requirement.'''
+    if isinstance(module_or_name_or_req, pkg_resources.Requirement):
+        return pkg_resources.working_set.find(module_or_name_or_req) or \
+                           pkg_resources.require(str(module_or_name_or_req))[0]
+    elif isinstance(module_or_name_or_req, types.ModuleType):
+        tasted_module = module_or_name_or_req
+    else:
+        assert isinstance(module_or_name_or_req, basestring)
+        try:
+            tasted_module = sys.modules[module_or_name_or_req]
+        except KeyError:
+            tasted_module = taste_module(module_or_name_or_req)
     loader = getattr(tasted_module, '__loader__', None)
     adapter = pkg_resources._find_adapter(
         pkg_resources._provider_factories,
--- garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_info_panel/image_display.py
+++ garlicsim_wx/garlicsim_wx/widgets/misc/simpack_selection_dialog/simpack_info_panel/image_display.py
@@ -10,8 +10,33 @@ See its documentation for more information.
 import wx
 
 from garlicsim.general_misc import caching
+from garlicsim.general_misc import module_tasting
 from garlicsim_wx.widgets.general_misc.cute_panel import CutePanel
 from garlicsim_wx.general_misc import wx_tools
+
+
+possible_image_names = [
+    'preview.png',
+    'preview.jpg',
+    'preview.gif'
+]
+
+
+@caching.cache()
+def get_simpack_bitmap(simpack_metadata):
+    for possible_image_name in possible_image_names:
+        if module_tasting.tasted_resources.resource_exists(
+            simpack_metadata._tasted_simpack,
+            possible_image_name
+        ):
+            stream = module_tasting.tasted_resources.resource_stream(
+                simpack_metadata._tasted_simpack,
+                possible_image_name
+            )
+            return wx.BitmapFromImage(wx.ImageFromStream(stream))
+            
+    else:
+        return None
 
 
 class ImageDisplay(CutePanel):
@@ -19,7 +44,9 @@ class ImageDisplay(CutePanel):
     def __init__(self, simpack_info_panel):
         self.simpack_info_panel = simpack_info_panel
         CutePanel.__init__(self, simpack_info_panel)
-        self.BackgroundColour = wx.NamedColour('red')
+        self._bitmap = wx.EmptyBitmap(1, 1)
+        #self.BackgroundColour = wx.NamedColour('red')
+        self.bind_event_handlers(BitmapViewer)
         self.Hide()
         
         
@@ -27,9 +54,20 @@ class ImageDisplay(CutePanel):
         simpack_metadata = \
             self.simpack_info_panel.simpack_selection_dialog.simpack_metadata
         if simpack_metadata is not None:
+            self._bitmap = get_simpack_bitmap(simpack_metadata)
             self.Show()
             self.Layout()
+            self.Refresh()
         else: # simpack_metadata is None
             self.Hide()
-            
+
         
+    def _on_set_focus(self, event):
+        event.Skip()
+        self.Navigate()
+
+        
+    def _on_paint(self, event):
+        dc = wx.PaintDC(self)
+        if self._bitmap:
+            dc.DrawBitmap(self._bitmap, 0, 0)
