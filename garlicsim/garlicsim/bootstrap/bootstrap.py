21fe5e9e6993f14d88c0fdd1ec98c345b75ec852
Ram Rachum <ram@rachum.com>
2011-09-28 14:14:29 +0200

    -

--- garlicsim/garlicsim/bootstrap/bootstrap.py
+++ garlicsim/garlicsim/bootstrap/bootstrap.py
@@ -22,6 +22,7 @@ if sys.version_info[1] <= 4:
 
 
 frozen = getattr(sys, 'frozen', None)
+is_pypy = ('__pypy__' in sys.builtin_module_names)
 
 
 def __check_prerequisites():
@@ -68,8 +69,8 @@ def __check_prerequisites():
             # Returning empty list because we didn't import `distribute`:
             return []
         
-    def check_pywin32():
-        if not sys.platform == 'win32':
+    def check_pywin32_on_cpython():
+        if not sys.platform == 'win32' or is_pypy: # todo: should check CPython
             return []
         try:
             import win32api
