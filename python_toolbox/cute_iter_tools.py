05117c0d597186f027e702102d197c9d9e1a7eea
Ram Rachum <ram@rachum.com>
2013-08-19 03:28:40 +0300

    -

--- python_toolbox/cute_iter_tools.py
+++ python_toolbox/cute_iter_tools.py
@@ -35,28 +35,25 @@ def get_consecutive_subsequences(iterable, length=2, wrap_around=False):
     
     iterator = iter(iterable)
     
-    first_items = get_items(iterator, length - 1, container=list)
+    first_items = get_items(iterator, length)
     if wrap_around:
-        len_first_items = len(first_items)
-        xrange_len_first_items = xrange(len(first_items))
-        if len(first_items) < length - 1:
-            for i in xrange_len_first_items: # to
-                yield (first_items[(i+j) % len_first_items] for j in
-                       xrange_len_first_items)
-        else:
-            iterator = itertools.chain(iterator, first_items)
+        if len(first_items) < length:
+            raise NotImplementedError(
+                '`length` is greater than the length of the iterable, and '
+                '`wrap_around` is set to `True`. Behavior for this is not '
+                'implemented, because it would require repeating some members '
+                'more than once.'
+            )
+        first_items_except_last = first_items[:-1]
+        iterator = itertools.chain(iterator, first_items_except_last)
             
     deque = collections.deque(first_items)
-    deque.appendleft(object()) # Filler to be removed on first iteration.
+    yield first_items
     
     if not wrap_around:
         # If `wrap_around` is `False`, we avoid holding a reference to items in
         # `first_items`, because they may need to be garbage-collected:
-        del first_items[:]
-        # We empty the sequence instead of deleting it because deleting it at
-        # this point would make for illegal Python syntax.
-    
-    yield tuple(first_items)
+        del first_items
     
     for current in iterator:
         deque.popleft()
--- test_python_toolbox/test_cute_iter_tools/test_get_consecutive_subsequences.py
+++ test_python_toolbox/test_cute_iter_tools/test_get_consecutive_subsequences.py
@@ -3,30 +3,136 @@
 
 '''Testing module for `cute_iter_tools.get_consecutive_subsequences`.'''
 
+from python_toolbox import gc_tools
 from python_toolbox import cute_testing
 from python_toolbox import sequence_tools
 
 from python_toolbox.cute_iter_tools import get_consecutive_subsequences
 
 
-def test():
-    '''Test basic workings of `cute_iter_tools.get_consecutive_subsequences`.'''
+def test_length_2():
     
-    # `consecutive_pairs` returns an iterator, not a sequence:
-    assert not sequence_tools.is_sequence(get_consecutive_subsequences(range(4)))
+    # `get_consecutive_subsequences` returns an iterator, not a sequence:
+    assert not sequence_tools.is_sequence(
+        get_consecutive_subsequences(range(4))
+    )
                                           
     assert tuple(get_consecutive_subsequences(range(4))) == \
            tuple(get_consecutive_subsequences(xrange(4))) == \
            ((0, 1), (1, 2), (2, 3))
                                           
     assert tuple(get_consecutive_subsequences(range(4), wrap_around=True)) == \
-           tuple(get_consecutive_subsequences(xrange(4), wrap_around=True)) == \
+           tuple(get_consecutive_subsequences(xrange(4), wrap_around=True)) ==\
            ((0, 1), (1, 2), (2, 3), (3, 0))
                                           
     assert tuple(get_consecutive_subsequences('meow')) == \
            (('m', 'e'), ('e', 'o'), ('o', 'w'))
     
-    assert tuple(get_consecutive_subsequences([1], wrap_around=True)) == \
-           ((1, 1),)
+    
+def test_iterable_too_short():
+    with cute_testing.RaiseAssertor(NotImplementedError):
+        tuple(get_consecutive_subsequences([1], wrap_around=True))
            
            
+def test_various_lengths():
+    assert tuple(get_consecutive_subsequences(xrange(7), length=3)) == \
+                        ((0, 1, 2), (1, 2, 3), (2, 3, 4), (3, 4, 5), (4, 5, 6))
+    assert tuple(get_consecutive_subsequences(xrange(7), length=4)) == \
+                       ((0, 1, 2, 3), (1, 2, 3, 4), (2, 3, 4, 5), (3, 4, 5, 6))
+    assert tuple(get_consecutive_subsequences(xrange(7), length=5)) == \
+                            ((0, 1, 2, 3, 4), (1, 2, 3, 4, 5), (2, 3, 4, 5, 6))
+    
+    assert tuple(get_consecutive_subsequences(xrange(7), length=4,
+            wrap_around=True)) == ((0, 1, 2, 3), (1, 2, 3, 4), (2, 3, 4, 5),
+            (3, 4, 5, 6), (4, 5, 6, 0), (5, 6, 0, 1), (6, 0, 1, 2))
+    assert tuple(get_consecutive_subsequences(xrange(7), length=5,
+            wrap_around=True)) == ((0, 1, 2, 3, 4), (1, 2, 3, 4, 5),
+            (2, 3, 4, 5, 6), (3, 4, 5, 6, 0), (4, 5, 6, 0, 1), (5, 6, 0, 1, 2),
+            (6, 0, 1, 2, 3))
+                                          
+           
+def test_garbage_collection():
+    
+    garbage_collected = set()
+
+    class GarbageNoter(object):
+        def __init__(self, n):
+            assert isinstance(n, int)
+            self.n = n
+        def __del__(self):
+            garbage_collected.add(self.n)
+            
+    iterable = (GarbageNoter(i) for i in xrange(7))
+    
+    consecutive_subsequences_iterator = \
+                               get_consecutive_subsequences(iterable, length=3)
+    
+    def assert_garbage_collected(indexes):
+        gc_tools.collect()
+        assert set(indexes) == garbage_collected
+        
+    assert_garbage_collected(())
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected(())
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((0,))
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((0, 1))
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((0, 1, 2))
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((0, 1, 2, 3))
+    with cute_testing.RaiseAssertor(StopIteration):
+        next(consecutive_subsequences_iterator)
+    assert_garbage_collected((0, 1, 2, 3, 4, 5, 6))
+        
+    
+    
+def test_garbage_collection_wrap_around():
+    
+    garbage_collected = set()
+
+    class GarbageNoter(object):
+        def __init__(self, n):
+            assert isinstance(n, int)
+            self.n = n
+        def __del__(self):
+            garbage_collected.add(self.n)
+            
+    iterable = (GarbageNoter(i) for i in xrange(7))
+    
+    consecutive_subsequences_iterator = \
+             get_consecutive_subsequences(iterable, length=3, wrap_around=True)
+    
+    def assert_garbage_collected(indexes):
+        gc_tools.collect()
+        assert set(indexes) == garbage_collected
+        
+    assert_garbage_collected(())
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected(())
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected(())
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected(())
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((2,))
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((2, 3))
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((2, 3, 4))
+    next(consecutive_subsequences_iterator)
+    assert_garbage_collected((2, 3, 4, 5))
+    with cute_testing.RaiseAssertor(StopIteration):
+        next(consecutive_subsequences_iterator)
+    assert_garbage_collected((0, 1, 2, 3, 4, 5, 6))
+        
+    
+    
+    
+            
+            
+    
+            
+            
+    
