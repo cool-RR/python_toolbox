ccebb5c87e36d3e49c1df44d1de1b6f305283b07
Ram Rachum <ram@rachum.com>
2013-09-03 01:30:42 +0300

    -

--- test_python_toolbox/test_nifty_collections/test_ordered_dict/test_with_stdlib_ordered_dict.py
+++ test_python_toolbox/test_nifty_collections/test_ordered_dict/test_with_stdlib_ordered_dict.py
@@ -8,13 +8,14 @@ import sys
 from python_toolbox import cute_testing
 
 from python_toolbox.nifty_collections.ordered_dict import OrderedDict
-from collections import OrderedDict as StdlibOrderedDict
 
 
 def test():
     if sys.version_info[:2] <= (2, 6):
         raise nose.SkipTest
-    
+
+    from collections import OrderedDict as StdlibOrderedDict
+
     ordered_dict = OrderedDict(((1, 'a'), (2, 'b'), (3, 'c')))
     stdlib_ordered_dict = StdlibOrderedDict(((1, 'a'), (2, 'b'), (3, 'c')))
     
