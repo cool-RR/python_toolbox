7eadd63dd9970e26d373634b3dc1daee7d67d65b
Ram Rachum <ram@rachum.com>
2013-09-14 21:18:18 +0300

    -

--- source_py2/python_toolbox/_bootstrap/bootstrap.py
+++ source_py2/python_toolbox/_bootstrap/bootstrap.py
@@ -6,17 +6,15 @@ import sys
 ### Confirming correct Python version: ########################################
 #                                                                             #
 if sys.version_info[0] >= 3:
-    raise Exception("Python 3.x is not supported, only Python 2.6 and Python "
-                    "2.7.")
+    raise Exception("This is a Python 2.x distribution of `python_toolbox`, "
+                    "and you're using Python 3.x. Please get the Python 3.x "
+                    "distribution.")
 if sys.version_info[1] <= 5:
     raise Exception(
         "You're using Python <= 2.5, but this package requires either Python "
-        "2.6 or Python 2.7, so you can't use it unless you upgrade your "
-        "Python version."
+        "2.6 or Python 2.7, (or Python 3.3+ on a different distribution,) so "
+        "you can't use it unless you upgrade your Python version."
     )
 #                                                                             #
 ### Finished confirming correct Python version. ###############################
 
-
-#frozen = getattr(sys, 'frozen', None)
-#is_pypy = ('__pypy__' in sys.builtin_module_names)
--- source_py3/python_toolbox/_bootstrap/bootstrap.py
+++ source_py3/python_toolbox/_bootstrap/bootstrap.py
@@ -5,18 +5,9 @@ import sys
 
 ### Confirming correct Python version: ########################################
 #                                                                             #
-if sys.version_info[0] >= 3:
-    raise Exception("Python 3.x is not supported, only Python 2.6 and Python "
-                    "2.7.")
-if sys.version_info[1] <= 5:
-    raise Exception(
-        "You're using Python <= 2.5, but this package requires either Python "
-        "2.6 or Python 2.7, so you can't use it unless you upgrade your "
-        "Python version."
-    )
+if sys.version_info[0] >= 2:
+    raise Exception("This is a Python 3.x distribution of `python_toolbox`, "
+                    "and you're using Python 2.x. Please get the Python 2.x "
+                    "distribution.")
 #                                                                             #
 ### Finished confirming correct Python version. ###############################
-
-
-#frozen = getattr(sys, 'frozen', None)
-#is_pypy = ('__pypy__' in sys.builtin_module_names)
