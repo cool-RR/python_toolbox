5220fe99e6d0b1d30be8b9795712514cbe03aec8
Ram Rachum <ram@rachum.com>
2013-09-14 21:31:22 +0300

    -

--- source_py2/python_toolbox/nifty_collections/ordered_dict.py
+++ source_py2/python_toolbox/nifty_collections/ordered_dict.py
@@ -12,136 +12,10 @@ from UserDict import DictMixin
 
 from python_toolbox import comparison_tools
 
-try:
-    from collections import OrderedDict as StdlibOrderedDict
-except ImportError:
-    StdlibOrderedDict = None
+from collections import OrderedDict as StdlibOrderedDict
 
 
-class OrderedDict(dict, DictMixin):
-    '''Dict that maintains order of items.'''
-
-    def __init__(self, *args, **kwds):
-        if len(args) > 1:
-            raise TypeError('expected at most 1 arguments, got %d' % len(args))
-        try:
-            self.__end
-        except AttributeError:
-            self.clear()
-        self.update(*args, **kwds)
-
-        
-    def clear(self):
-        '''D.clear() -> None.  Remove all items from D.'''
-        self.__end = end = []
-        end += [None, end, end]         # sentinel node for doubly linked list
-        self.__map = {}                 # key --> [key, prev, next]
-        dict.clear(self)
-
-        
-    def __setitem__(self, key, value):
-        if key not in self:
-            end = self.__end
-            current = end[1]
-            current[2] = end[1] = self.__map[key] = [key, current, end]
-        dict.__setitem__(self, key, value)
-
-        
-    def __delitem__(self, key):
-        dict.__delitem__(self, key)
-        key, prev, next = self.__map.pop(key)
-        prev[2] = next
-        next[1] = prev
-
-        
-    def __iter__(self):
-        end = self.__end
-        current = end[2]
-        while current is not end:
-            yield current[0]
-            current = current[2]
-
-            
-    def __reversed__(self):
-        end = self.__end
-        current = end[1]
-        while current is not end:
-            yield current[0]
-            current = current[1]
-
-            
-    def popitem(self, last=True):
-        '''
-        D.popitem() -> (k, v), remove and return some (key, value) pair as a
-        2-tuple; but raise KeyError if D is empty
-        '''
-        if not self:
-            raise KeyError('dictionary is empty')
-        if last:
-            key = reversed(self).next()
-        else:
-            key = iter(self).next()
-        value = self.pop(key)
-        return key, value
-
-    
-    def __reduce__(self):
-        items = [[k, self[k]] for k in self]
-        tmp = self.__map, self.__end
-        del self.__map, self.__end
-        inst_dict = vars(self).copy()
-        self.__map, self.__end = tmp
-        if inst_dict:
-            return (self.__class__, (items,), inst_dict)
-        return self.__class__, (items,)
-
-    
-    def keys(self):
-        return list(self)
-
-    
-    setdefault = DictMixin.setdefault
-    update = DictMixin.update
-    pop = DictMixin.pop
-    values = DictMixin.values
-    items = DictMixin.items
-    iterkeys = DictMixin.iterkeys
-    itervalues = DictMixin.itervalues
-    iteritems = DictMixin.iteritems
-
-    
-    def __repr__(self):
-        if not self:
-            return '%s()' % (self.__class__.__name__,)
-        return '%s(%r)' % (self.__class__.__name__, self.items())
-
-    
-    def copy(self):
-        return self.__class__(self)
-
-    
-    @classmethod
-    def fromkeys(cls, iterable, value=None):
-        d = cls()
-        for key in iterable:
-            d[key] = value
-        return d
-
-    
-    def __eq__(self, other):
-        if isinstance(other, _ordered_dict_types):
-            if len(self) != len(other):
-                return False
-            for p, q in zip(self.items(), other.items()):
-                if p != q:
-                    return False
-            return True
-        return dict.__eq__(self, other)
-
-    
-    def __ne__(self, other):
-        return not self == other
-
+class OrderedDict(StdlibOrderedDict):
     
     def move_to_end(self, key, last=True):
         '''Move an existing element to the end (or beginning if last==False).
@@ -192,5 +66,3 @@ class OrderedDict(dict, DictMixin):
                 return i
         raise RuntimeError
                 
-                
-_ordered_dict_types = filter(bool, (OrderedDict, StdlibOrderedDict))
