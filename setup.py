fb8fc68e1e10bf9cc91826a431dd14f308f2389d
Ram Rachum <ram@rachum.com>
2013-09-19 01:42:07 +0300

    -

--- setup.py
+++ setup.py
@@ -140,7 +140,6 @@ my_classifiers = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Utilities'
 ]
