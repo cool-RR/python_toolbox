d2b547737438776772fe8f38893764df19ec6364
Ram Rachum <ram@rachum.com>
2013-10-25 22:01:39 +0300

    -

---
+++ source_py3/test_python_toolbox/test_cute_iter_tools/test_get_single_if_any.py
@@ -0,0 +1,25 @@
+# Copyright 2009-2013 Ram Rachum.
+# This program is distributed under the MIT license.
+
+from python_toolbox import cute_testing
+
+from python_toolbox.cute_iter_tools import get_single_if_any
+
+
+def test_get_single_if_any():
+    
+    assert get_single_if_any(()) is get_single_if_any([]) is \
+           get_single_if_any({}) is get_single_if_any(iter({})) is \
+           get_single_if_any('') is None
+    
+    assert get_single_if_any(('g',)) == get_single_if_any(['g']) == \
+           get_single_if_any({'g'}) == get_single_if_any(iter({'g'})) == \
+           get_single_if_any('g') == 'g'
+
+    with cute_testing.RaiseAssertor():
+        get_single_if_any(('g', 'e', 'e'))
+
+    with cute_testing.RaiseAssertor():
+        get_single_if_any('gee')
+        
+    assert get_single_if_any('gee', exception_on_multiple=None) == 'g'
