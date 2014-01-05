b5dbae1003eeeeb3c19b0c4f1fd15647925cdbc2
Ram Rachum <ram@rachum.com>
2014-01-06 00:13:28 +0200

    -

--- source_py3/python_toolbox/file_tools.py
+++ source_py3/python_toolbox/file_tools.py
@@ -5,7 +5,12 @@
 
 import pathlib
 import re
+import contextlib
 
+from python_toolbox import context_management
+
+
+N_MAX_ATTEMPTS = 100
 
 numbered_name_pattern = re.compile(
     r'''(?P<raw_name>.*) \((?P<number>[0-9]+)\)'''
@@ -16,7 +21,7 @@ def _get_next_name(path):
     parent = path.parent
     suffix = path.suffix
     suffixless_name = path.name[:-len(suffix)]
-    assert parent + suffix + suffixless_name == path
+    assert '{}{}{}'.format(parent, suffixless_name, suffix) == path
     match = numbered_name_pattern.match(suffixless_name)
     if match:
         fixed_suffixless_name = '{} ({})'.format(
@@ -31,8 +36,43 @@ def _get_next_name(path):
     )
     
     
+class FileContainer(context_management.ContextManager):
+    def __init__(self, file):
+        self.file = file
+        
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        return self.file.__exit__(exc_type, exc_value, exc_traceback)
+        
 
-def create_file_renaming_if_taken(path, binary=False):
-    path = pathlib.Path(path)
+def create_file_renaming_if_taken(path, mode='x',
+                                  buffering=-1, encoding=None,
+                                  errors=None, newline=None):
+    assert 'x' in mode
+    current_path = pathlib.Path(path)
+    with contextlib.ExitStack() as exit_stack:
+        for i in range(N_MAX_ATTEMPTS):
+            try:
+                file = exit_stack.enter_context(
+                    current_path.open(mode, buffering=buffering,
+                                      encoding=encoding, errors=errors,
+                                      newline=newline)
+                )
+            except FileExistsError:
+                current_path = _get_next_name(current_path)
+            else:
+                return FileContainer(file)
+        else:
+            raise Exception("Exceeded {} tries, can't create file {}".format(
+                N_MAX_ATTEMPTS,
+                path
+            ))
     
 
+def write_to_file_renaming_if_taken(path, data, mode='x',
+                                    buffering=-1, encoding=None,
+                                    errors=None, newline=None):
+    with create_file_renaming_if_taken(
+        path, mode=mode, buffering=buffering, encoding=encoding, errors=errors,
+        newline=newline) as file:
+        file.write(data)
+        
