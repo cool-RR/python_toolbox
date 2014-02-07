e64d563dd18883ccf056c70cf7daa7c8d54c8cdd
Ram Rachum <ram@rachum.com>
2014-02-08 00:28:02 +0200

    -

--- source_py3/python_toolbox/nifty_collections/lazy_tuple.py
+++ source_py3/python_toolbox/nifty_collections/lazy_tuple.py
@@ -176,7 +176,7 @@ class LazyTuple(collections.Sequence, object):
     def __eq__(self, other):
         if not sequence_tools.is_immutable_sequence(other):
             return False
-        for i, j in itertools.izip_longest(self, other,
+        for i, j in itertools.zip_longest(self, other,
                                            fillvalue=_SENTINEL):
             if (i is _SENTINEL) or (j is _SENTINEL):
                 return False
@@ -201,7 +201,7 @@ class LazyTuple(collections.Sequence, object):
             return False
         elif not self and not other:
             return False
-        for a, b in itertools.izip_longest(self, other,
+        for a, b in itertools.zip_longest(self, other,
                                            fillvalue=_SENTINEL):
             if a is _SENTINEL:
                 # `self` ran out. Now there can be two cases: (a) `other` ran
