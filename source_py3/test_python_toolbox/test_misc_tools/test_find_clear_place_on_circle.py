2d7ba3bec59e5705d61cf4d491e7b89ff6325900
Ram Rachum <ram@rachum.com>
2014-03-30 17:24:12 +0300

    -

--- source_py3/test_python_toolbox/test_misc_tools/test_find_clear_place_on_circle.py
+++ source_py3/test_python_toolbox/test_misc_tools/test_find_clear_place_on_circle.py
@@ -1,8 +1,6 @@
 # Copyright 2009-2014 Ram Rachum.
 # This program is distributed under the MIT license.
 
-'''Testing module for `find_clear_place_on_circle`.'''
-
 import nose.tools
 
 from python_toolbox.misc_tools import find_clear_place_on_circle
