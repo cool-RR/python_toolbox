1180b1f65861f65a11a2615ee5a1ddf8feee7267
Ram Rachum <ram@rachum.com>
2014-09-21 01:38:53 +0300

    -

--- source_py3/test_python_toolbox/test_combi/test_chain_space.py
+++ source_py3/test_python_toolbox/test_combi/test_chain_space.py
@@ -8,8 +8,10 @@ def test_chain_spaces():
     chain_space = ChainSpace((range(3), 'meow', range(2, -1, -1)))
     assert tuple(chain_space) == (0, 1, 2, 'm', 'e', 'o', 'w', 2, 1, 0)
     assert len(chain_space) == chain_space.length == 10
+    assert bool(chain_space) is True
     for i, item in enumerate(chain_space):
         assert chain_space[i] == item
+        assert chain_space.index(item) == i
         
     assert chain_space == chain_space
     
