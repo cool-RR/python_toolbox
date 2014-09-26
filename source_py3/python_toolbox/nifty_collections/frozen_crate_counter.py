03c2edcbbe6635891dd35199c1d6084ec4fa00d9
Ram Rachum <ram@rachum.com>
2014-09-26 22:42:12 +0300

    -

--- source_py3/python_toolbox/nifty_collections/frozen_crate_counter.py
+++ source_py3/python_toolbox/nifty_collections/frozen_crate_counter.py
@@ -36,16 +36,21 @@ class FrozenCrateCounter(FrozenCounter):
             
     def get_sub_counters_for_one_crate_and_previous_piles_removed(self):
         sub_counters = []
-        d = {}
-        # sorted_items = sorted(self.items())
+        growing_dict = {}
         for key_to_reduce, value_of_key_to_reduce in \
                                                 reversed(sorted(self.items())):
-            d[key_to_reduce] = value_of_key_to_reduce - 1
-
-            sub_counter = collections.Counter(self)
-            sub_counter[key_to_reduce] -= 1
-            sub_counter[key_to_reduce - 1] += 1
-            sub_counters_counter[FrozenCrateCounter(sub_counter)] = \
-                                                         value_of_key_to_reduce
+            growing_dict[key_to_reduce] = value_of_key_to_reduce
+            
+            sub_counter_prototype = collections.Counter(growing_dict)
+            sub_counter_prototype[key_to_reduce] -= 1
+            sub_counter_prototype[key_to_reduce - 1] += 1
+            
+            for i in range(value_of_key_to_reduce):
+                sub_counters.append(
+                    FrozenCrateCounter(
+                        {key: ((value - i) if key == key_to_reduce else value)
+                                       for key, value in sub_counter_prototype}
+                    )
+                )
         return tuple(sub_counters)
             
