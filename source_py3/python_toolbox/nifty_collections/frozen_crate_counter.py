28e07c01f6dff5d79f51a8e7df48e7093138e80c
Ram Rachum <ram@rachum.com>
2014-09-26 22:13:55 +0300

    -

--- source_py3/python_toolbox/nifty_collections/frozen_crate_counter.py
+++ source_py3/python_toolbox/nifty_collections/frozen_crate_counter.py
@@ -8,7 +8,10 @@ from python_toolbox import math_tools
 from .frozen_counter import FrozenCounter
 
 
-class FrozenChunkCounter(FrozenCounter):
+class FrozenCrateCounter(FrozenCounter):
+    '''
+    blocktododoc do entire crate metaphor with drawings
+    '''
     def __init__(self, iterable):
         super().__init__(iterable)
         
@@ -21,13 +24,24 @@ class FrozenChunkCounter(FrozenCounter):
                 raise TypeError('Keys to `FrozenChunkCounter` must be '
                                 'non-negative integers.')
             
-    def get_sub_counters_counter(self):
+    def get_sub_counters_for_one_crate_removed(self):
         sub_counters_counter = collections.Counter()
         for key_to_reduce, value_of_key_to_reduce in self.items():
             sub_counter = collections.Counter(self)
             sub_counter[key_to_reduce] -= 1
             sub_counter[key_to_reduce - 1] += 1
-            sub_counters_counter[FrozenChunkCounter(sub_counter)] = \
+            sub_counters_counter[FrozenCrateCounter(sub_counter)] = \
+                                                         value_of_key_to_reduce
+        return FrozenCounter(sub_counters_counter)
+            
+    def get_sub_counters_for_one_crate_removed_and_previous_crates_destroed(
+                                                                         self):
+        sub_counters_counter = collections.Counter()
+        for key_to_reduce, value_of_key_to_reduce in self.items():
+            sub_counter = collections.Counter(self)
+            sub_counter[key_to_reduce] -= 1
+            sub_counter[key_to_reduce - 1] += 1
+            sub_counters_counter[FrozenCrateCounter(sub_counter)] = \
                                                          value_of_key_to_reduce
         return FrozenCounter(sub_counters_counter)
             
