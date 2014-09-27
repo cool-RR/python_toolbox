64d578b54e3bb24fe862bad2aa45918b73663e3c
Ram Rachum <ram@rachum.com>
2014-09-27 05:07:50 +0300

    -

--- source_py3/python_toolbox/combi/variations.py
+++ source_py3/python_toolbox/combi/variations.py
@@ -1,7 +1,9 @@
 import enum
 
 from python_toolbox import cute_iter_tools
+from python_toolbox import nifty_collections
 from python_toolbox import caching
+from python_toolbox.third_party import sortedcontainers
 
 from .selection_space import SelectionSpace
 
@@ -21,7 +23,9 @@ class PermSpaceVariation(enum.Enum):
     FIXED = 'fixed'
     DEGREED = 'degreed'
     SLICED = 'sliced'
-
+    
+        
+PermSpaceVariation.index = tuple(PermSpaceVariation).index
 
 variation_clashes = (
     {PermSpaceVariation.DEGREED, PermSpaceVariation.COMBINATION},
@@ -31,13 +35,38 @@ variation_clashes = (
 )
 
 
-allowed_variation_combinations, unallowed_variation_combinations = \
-                                                 cute_iter_tools.double_filter(
-    lambda variation_combination: any(
-                variation_clash <= variation_combination for variation_clash in
-                                                            variation_clashes),
-    SelectionSpace(PermSpaceVariation),
-    lazy_tuple=True
-)
-
-
+class VariationSelectionSpace(SelectionSpace):
+    def __init__(self):
+        SelectionSpace.__init__(self, PermSpaceVariation)
+        
+    @caching.cache()
+    def __getitem__(self, i):
+        return VariationSelection(SelectionSpace.__getitem__(self, i))
+        
+    @caching.cache()
+    def __repr__(self):
+        return '<VariationSelectionSpace>'
+    
+        
+class VariationSelection:
+    def __init__(self, variations):
+        self.variations = variations
+        
+    @caching.cache()
+    def __repr__(self):
+        return '<%s: %s>' % (
+            type(self).__name__,
+            ', '.join(variation.value for variation in
+                      sorted(self.variations, key=PermSpaceVariation.index))
+        )
+    
+    @caching.CachedProperty
+    def is_allowed(self):
+        return not any(variation_clash <= self.variations for
+                       variation_clash in variation_clashes)
+    
+     
+variation_selection_space = VariationSelectionSpace()
+variation_selection_space[7].is_allowed
+repr(variation_selection_space[7])
+2 / 2
