6fa642fc2532d93a8755b7e82ff71e6f3754d0b8
Ram Rachum <ram@rachum.com>
2014-10-07 21:10:07 +0300

    -

--- source_py3/python_toolbox/nifty_collections/abstract.py
+++ source_py3/python_toolbox/nifty_collections/abstract.py
@@ -4,6 +4,8 @@ import queue
 import multiprocessing.queues
 
 
+###############################################################################
+
 class Ordered(metaclass=abc.ABCMeta):
     __slots__ = ()
 
@@ -13,3 +15,24 @@ Ordered.register(collections.OrderedDict)
 Ordered.register(collections.deque)
 Ordered.register(queue.Queue)
 Ordered.register(multiprocessing.queues.Queue)
+
+###############################################################################
+
+class DefinitelyUnordered(metaclass=abc.ABCMeta):
+    __slots__ = ()
+    
+    @classmethod
+    def __subclasshook__(cls, type_):
+        if cls is DefinitelyUnordered and \
+                                    issubclass(type_, collections.OrderedDict):
+            return False
+        else:
+            return NotImplemented
+        
+
+DefinitelyUnordered.register(set)
+DefinitelyUnordered.register(frozenset)
+DefinitelyUnordered.register(dict)
+DefinitelyUnordered.register(collections.defaultdict)
+DefinitelyUnordered.register(collections.Counter)
+DefinitelyUnordered.register
