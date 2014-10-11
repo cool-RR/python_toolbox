db29db5362abc99c0fd548286b38e456f6dab980
Ram Rachum <ram@rachum.com>
2014-10-11 18:03:42 +0300

    -

--- source_py3/python_toolbox/combi/__init__.py
+++ source_py3/python_toolbox/combi/__init__.py
@@ -1,6 +1,9 @@
 import math
 from python_toolbox.math_tools import binomial # Making it easy to find
 
+from python_toolbox.nifty_collections import (Bag, OrderedBag, FrozenBag,
+                                              FrozenOrderedBag)
+
 from .chain_space import ChainSpace
 from .product_space import ProductSpace
 from .map_space import MapSpace
