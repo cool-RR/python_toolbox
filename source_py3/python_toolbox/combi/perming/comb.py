78f50e24a231ced90159a8aa429a8fb8e956aced
Ram Rachum <ram@rachum.com>
2014-10-13 13:45:06 +0300

    -

--- source_py3/python_toolbox/combi/perming/comb.py
+++ source_py3/python_toolbox/combi/perming/comb.py
@@ -4,8 +4,13 @@ from .comb_space import CombSpace
         
 class Comb(Perm):
     '''blocktododoc'''
-    def __init__(self, perm_sequence, perm_space):
-        assert isinstance(perm_space, CombSpace)
+    def __init__(self, perm_sequence, perm_space=None):
+        # Unlike for `Perm`, we must have a `perm_space` in the arguments. It
+        # can either be in the `perm_space` argument, or if the `perm_sequence`
+        # we got is a `Comb`, then we'll take the one from it.
+        assert isinstance(perm_space, CombSpace) or \
+                                                isinstance(perm_sequence, Comb)
+        
         Perm.__init__(self, perm_sequence=perm_sequence,
                       perm_space=perm_space)
         
