dad2cc3bc30a23399ff2c83b38d120afd8e72f2d
Ram Rachum <ram@rachum.com>
2014-10-18 14:09:16 +0300

    -

--- source_py3/test_python_toolbox/test_zip_tools/test_zip_folder.py
+++ source_py3/test_python_toolbox/test_zip_tools/test_zip_folder.py
@@ -26,7 +26,9 @@ def test():
         zip_tools.zip_folder(folder_to_zip, temp_folder / 'archive.zip')
         
         assert zip_file_path.is_file()
-        assert zip_tools.unzip_in_memory(zip_file_path.open('rb').read()) == (
+        assert set(
+            zip_tools.unzip_in_memory(zip_file_path.open('rb').read())
+        ) == {
             ('folder_to_zip/some_file.txt', b'hello there!'), 
             ('folder_to_zip/some_other_file.txt', b'hello there again!'), 
-        )
+        }
