132f4e62b22151b1b8fa5cf1c9428488ebf9d31f
Ram Rachum <ram@rachum.com>
2014-10-19 21:55:58 +0300

    -

--- source_py3/python_toolbox/math_tools/types.py
+++ source_py3/python_toolbox/math_tools/types.py
@@ -13,18 +13,18 @@ class _PossiblyInfiniteIntegralType(abc.ABCMeta):
         return isinstance(thing, numbers.Integral) or (thing in infinities)
 class PossiblyInfiniteIntegral(numbers.Number,
                                metaclass=_PossiblyInfiniteIntegralType):
-    pass
+    '''An integer or infinity (including negative infinity.)'''
 
 class _PossiblyInfiniteRealType(abc.ABCMeta):
     def __instancecheck__(self, thing):
         return isinstance(thing, numbers.Real) or (thing in infinities)
 class PossiblyInfiniteReal(numbers.Number,
                            metaclass=_PossiblyInfiniteRealType):
-    pass
+    '''A real number or infinity (including negative infinity.)'''
 
 class _NaturalType(abc.ABCMeta):
     def __instancecheck__(self, thing):
         return isinstance(thing, numbers.Integral) and thing >= 1
 class Natural(numbers.Number,
               metaclass=_NaturalType):
-    pass
+    '''A natural number, meaning a positive integer (0 not included.)'''
