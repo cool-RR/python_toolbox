3c65e21bf2e6df2457feb48aff31b535c6bf2a51
Ram Rachum <ram@rachum.com>
2014-10-22 16:45:22 +0300

    -

--- source_py2/test_python_toolbox/test_zip_tools/test_zip_folder.py
+++ source_py2/test_python_toolbox/test_zip_tools/test_zip_folder.py
@@ -24,14 +24,23 @@ def test():
         (folder_to_zip / 'some_other_file.txt').open('w').write(
                                                          u'hello there again!')
         
+        import gc; gc.collect()
+        
         zip_file_path = temp_folder / 'archive.zip'
         assert isinstance(zip_file_path, pathlib.Path)
         zip_tools.zip_folder(folder_to_zip, temp_folder / 'archive.zip')
         
-        assert zip_file_path.is_file()
-        assert set(
+        result = set(
             zip_tools.unzip_in_memory(zip_file_path.open('rb').read())
-        ) == {
+        )
+        
+        assert zip_file_path.is_file()
+        
+        # Got two options here because of PyPy shenanigans:
+        assert result == {
             ('folder_to_zip/some_file.txt', b'hello there!'), 
             ('folder_to_zip/some_other_file.txt', b'hello there again!'), 
+        } or result == {
+            ('folder_to_zip/some_file.txt', 'hello there!'), 
+            ('folder_to_zip/some_other_file.txt', 'hello there again!'), 
         }
