8c2370ab6b037463352c44c971afc4952d7ce2d6
Ram Rachum <ram@rachum.com>
2015-06-09 19:58:18 +0300

    -

--- source_py2/python_toolbox/nifty_collections/ordered_dict.py
+++ source_py2/python_toolbox/nifty_collections/ordered_dict.py
@@ -1,6 +1,7 @@
 # Copyright 2009-2015 Ram Rachum.
 # This program is distributed under the MIT license.
 
+from python_toolbox import sys_tools
 from python_toolbox import comparison_tools
 
 from collections import OrderedDict as StdlibOrderedDict
@@ -21,22 +22,30 @@ class OrderedDict(StdlibOrderedDict):
         When last=True, acts like a fast version of self[key]=self.pop(key).
 
         '''
-        link = self.__map[key]
-        link_prev = link[0]
-        link_next = link[1]
-        link_prev[1] = link_next
-        link_next[0] = link_prev
-        root = self.__root
-        if last:
-            last = self.__root[0]
-            link[0] = last
-            link[1] = self.__root
-            last[1] = self.__root[0] = link
+        if sys_tools.is_pypy:
+            if last is False:
+                raise NotImplementedError
+            value = self[key]
+            del self[key]
+            self[key] = value
+            return
         else:
-            first = self.__root[1]
-            link[0] = self.__root
-            link[1] = first
-            root[1] = first[0] = link
+            link = self.__map[key]
+            link_prev = link[0]
+            link_next = link[1]
+            link_prev[1] = link_next
+            link_next[0] = link_prev
+            root = self.__root
+            if last:
+                last = self.__root[0]
+                link[0] = last
+                link[1] = self.__root
+                last[1] = self.__root[0] = link
+            else:
+                first = self.__root[1]
+                link[0] = self.__root
+                link[1] = first
+                root[1] = first[0] = link
             
 
     def sort(self, key=None, reverse=False):
