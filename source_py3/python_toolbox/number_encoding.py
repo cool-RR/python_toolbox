43524fb3e51e86ce27f6801c2d966301f61ecae1
Ram Rachum <ram@rachum.com>
2015-12-13 23:13:38 +0200

    -

--- source_py3/python_toolbox/number_encoding.py
+++ source_py3/python_toolbox/number_encoding.py
@@ -5,6 +5,20 @@ from python_toolbox import sequence_tools
 
 
 class NumberEncoder:
+    '''
+    A very simple encoders between lines and strings.
+    
+    Example:
+    
+        >>> my_encoder = number_encoding.NumberEncoder('isogram')
+        >>> my_encoder.encode(10000)
+        'rssir'
+        >>> my_encoder.encode(10000000)
+        'saimmmgrg'
+        >>> my_encoder.decode('saimmmgrg')
+        10000000
+
+    '''
     def __init__(self, characters):
         self.characters = \
                sequence_tools.ensure_iterable_is_immutable_sequence(characters)
@@ -12,7 +26,13 @@ class NumberEncoder:
         if recurrences:
             raise Exception('`characters` must not have recurring characters.')
         
-    def encode(number, minimum_length=1):
+    def encode(self, number, minimum_length=1):
+        '''
+        Encode the number into a string.
+        
+        If `minimum_length > 1`, the string will be padded (with the "zero"
+        character) if the number isn't big enough.
+        '''
         current_number = number
         result = ''
         while current_number:
@@ -24,6 +44,12 @@ class NumberEncoder:
         return result
 
     def decode(self, string):
+        '''Decode `string` into a number'''
+        
         assert isinstance(string, (str, bytes))
         return sum((len(self.characters)**i) * self.characters.index(x)
                                          for (i, x) in enumerate(string[::-1]))
+
+    def __repr__(self):
+        return '<%s: %s>' % (type(self).__name__, repr(self.characters))
+        
