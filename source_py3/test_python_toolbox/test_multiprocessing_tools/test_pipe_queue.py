bb4058d54bce626cd7a6b426a307bd4aae6c2b33
Ram Rachum <ram@rachum.com>
2016-07-21 18:44:17 +0300

    --

--- source_py3/test_python_toolbox/test_multiprocessing_tools/test_pipe_queue.py
+++ source_py3/test_python_toolbox/test_multiprocessing_tools/test_pipe_queue.py
@@ -1,6 +1,7 @@
 # Copyright 2009-2017 Ram Rachum.
 # This program is distributed under the MIT license.
 
+import time
 import multiprocessing
 
 from python_toolbox import multiprocessing_tools
@@ -21,4 +22,40 @@ class Worker(multiprocessing.Process):
 def test():
     pipe_queue = multiprocessing_tools.PipeQueue()
     workers = [Worker(pipe_queue) for _ in range(3)]
-    1 / 0 was here
+    def get_n_alive_workers():
+        return len([worker.alive() for worker in workers])
+    def assert_n_live_workers(n):
+        for i in range(5):
+            if get_n_alive_workers() == n:
+                return
+            else:
+                time.sleep(i)
+        else:
+            raise AssertionError
+        
+    connection_1 = pipe_queue.create_pipe()
+    connection_1.send('Hello worker!')
+    for worker in workers:
+        worker.start()
+    assert_n_live_workers(3)
+    connection_2 = pipe_queue.create_pipe()
+    connection_2.send('Hello worker!')
+    message = connection_1.recv()
+    assert message == 'Why hello there master!'
+    message = connection_2.recv()
+    assert message == 'Why hello there master!'
+    assert_n_live_workers(3)
+    connection_1.send('Please exit mister worker!')
+    assert_n_live_workers(2)
+    connection_2.send('Please exit mister worker!')
+    connection_3 = pipe_queue.create_pipe()
+    assert_n_live_workers(1)
+    connection_3.send('Hello worker!')
+    assert_n_live_workers(1)
+    message = connection_3.recv()
+    assert message == 'Why hello there master!'
+    connection_3.send('Please exit mister worker!')
+    assert_n_live_workers(1)
+        
+    
+    
