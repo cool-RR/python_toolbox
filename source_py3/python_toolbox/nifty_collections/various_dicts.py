6dfd24e7730692499c019e125f76a790dc8c5756
Ram Rachum <ram@rachum.com>
2016-08-10 01:26:49 +0300

    dicts

--- source_py3/python_toolbox/nifty_collections/various_dicts.py
+++ source_py3/python_toolbox/nifty_collections/various_dicts.py
@@ -6,24 +6,33 @@ import operator
 import functools
 import itertools
 
+from python_toolbox import comparison_tools
+
 from .abstract import Ordered, DefinitelyUnordered
 from .ordered_dict import OrderedDict
 
 
-class _AbstractFrozenDict(collections.abc.Mapping):
-    _hash = None # Overridden by instance when calculating hash.
 
+class _AbstractMappingDelegator(collections.abc.Mapping):
     def __init__(self, *args, **kwargs):
         self._dict = self._dict_type(*args, **kwargs)
 
     __getitem__ = lambda self, key: self._dict[key]
     __len__ = lambda self: len(self._dict)
     __iter__ = lambda self: iter(self._dict)
-
+        
     def copy(self, *args, **kwargs):
         base_dict = self._dict.copy()
         base_dict.update(*args, **kwargs)
         return type(self)(base_dict)
+
+    __repr__ = lambda self: '%s(%s)' % (type(self).__name__,
+                                        repr(self._dict) if self._dict else '')
+    __reduce__ = lambda self: (self.__class__ , (self._dict,))
+
+
+class _AbstractFrozenDict(_AbstractMappingDelegator):
+    _hash = None # Overridden by instance when calculating hash.
     
     def __hash__(self):
         if self._hash is None:
@@ -40,12 +49,50 @@ class _AbstractFrozenDict(collections.abc.Mapping):
             )
 
         return self._hash
-    
-    __repr__ = lambda self: '%s(%s)' % (type(self).__name__,
-                                        repr(self._dict) if self._dict else '')
-    __reduce__ = lambda self: (self.__class__ , (self._dict,))
+
+class _AbstractDoubleSidedDict(collections.abc.Mapping):
+    def __init__(self, *args, **kwargs):
+        self._dict = self._dict_type(*args, **kwargs)
 
     
+class OrderedDict(collections.OrderedDict):
+    '''
+    A dictionary with an order.
+    
+    This is a subclass of `collections.OrderedDict` with a couple of
+    improvements.
+    '''
+    
+    def sort(self, key=None, reverse=False):
+        '''
+        Sort the items according to their keys, changing the order in-place.
+        
+        The optional `key` argument, (not to be confused with the dictionary
+        keys,) will be passed to the `sorted` function as a key function.
+        '''
+        key_function = \
+                   comparison_tools.process_key_function_or_attribute_name(key)
+        sorted_keys = sorted(self.keys(), key=key_function, reverse=reverse)
+        for key_ in sorted_keys[1:]:
+            self.move_to_end(key_)
+        
+    
+    def index(self, key):
+        '''Get the index number of `key`.'''
+        if key not in self:
+            raise ValueError
+        for i, key_ in enumerate(self):
+            if key_ == key:
+                return i
+        raise RuntimeError
+    
+    @property
+    def reversed(self):
+        '''Get a version of this `OrderedDict` with key order reversed.'''
+        return type(self)(reversed(tuple(self.items())))
+    
+
+
 class FrozenDict(DefinitelyUnordered, _AbstractFrozenDict):
     '''
     An immutable `dict`.
@@ -100,3 +147,14 @@ class FrozenOrderedDict(Ordered, _AbstractFrozenDict):
             inner = ''
         return '%s(%s)' % (type(self).__name__, inner)
     
+    
+        
+    
+class DoubleSidedFrozenDict:
+    1 / 0
+    
+class DoubleSidedOrderedDict:
+    1 / 0
+    
+class DoubleSidedFrozenOrderedDict:
+    1 / 0
