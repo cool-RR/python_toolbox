1e170e70c2527c14046f8849e99a2b1580d5102f
Ram Rachum <ram@rachum.com>
2019-07-18 20:53:45 +0300

    -

--- setup.py
+++ setup.py
@@ -105,10 +105,9 @@ my_classifiers = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Utilities'
 ]
--- test_python_toolbox/test_import_tools/test_exists/test_zip.py
+++ test_python_toolbox/test_import_tools/test_exists/test_zip.py
@@ -23,13 +23,23 @@ from . import resources as __resources_package
 resources_package = __resources_package.__name__
 
 
+zip_string = (
+    b'PK\x03\x04\x14\x00\x00\x00\x08\x00\xd0cI>c\xad\x8e3U\x00\x00\x00b\x00'
+    b'\x00\x00\x1e\x00\x00\x00zip_imported_module_bla_bla.py\x1d\xcbA\x0e'
+    b"@@\x0c\x05\xd0\xab\xfc]W\xe6\x02\xce\xe0\x0c\x1d\xa1h2\xa62-\x11\xa7'"
+    b'\xf6\xef\x11\xd1`\xf3Y\x04\x8b5\x84xh]\x91u?\xac\x05\x87Y\xf1$\xb7zx\x86'
+    b'U<ztc\x9b6\xbdd\xc6\xfeGOD\xd4ct\x97\x16\xa0\xf4\x11\x82V0/Z\x84\xf9'
+    b'\x05PK\x01\x02\x17\x0b\x14\x00\x00\x00\x08\x00\xd0cI>c\xad\x8e3U\x00\x00'
+    b'\x00b\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x00\x00\x01\x00 \x00\x00\x00'
+    b'\x00\x00\x00\x00zip_imported_module_bla_bla.pyPK\x05\x06\x00\x00\x00'
+    b'\x00\x01\x00\x01\x00L\x00\x00\x00\x91\x00\x00\x00\x00\x00'
+)
+
+
 def test_zip():
     '''Test `exists` works on zip-imported modules.'''
 
     assert not exists('zip_imported_module_bla_bla')
-
-    zip_string = pkg_resources.resource_string(resources_package,
-                                               'archive_with_module.zip')
 
     with temp_file_tools.create_temp_folder(
                                  prefix='test_python_toolbox_') as temp_folder:
