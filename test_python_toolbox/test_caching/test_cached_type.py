bc910fab76b47dfcc96dfe9cfdaaea029b2845d0
Ram Rachum <ram@rachum.com>
2019-07-20 17:00:35 +0300

    -

--- test_python_toolbox/test_caching/test_cached_type.py
+++ test_python_toolbox/test_caching/test_cached_type.py
@@ -14,3 +14,10 @@ def test():
 
     assert A() is A(1) is A(b=2) is A(1, 2) is A(1, b=2)
     assert A() is not A(3) is not A(b=7) is not A(1, 2, 'meow') is not A(x=9)
+
+def test_keyword_only_separator_and_annotations():
+    class B(metaclass=CachedType):
+        def __init__(self, a: int, b: float, *, c: 'lol' = 7) -> None:
+            pass
+
+    assert B(1, 2) is B(b=2, a=1, c=7) is not B(b=2, a=1, c=8)
