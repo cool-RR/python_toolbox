68f4aa6d1970c3864a5850e92f161d540023e48c
Ram Rachum <ram@rachum.com>
2020-05-21 12:36:55 +0300

    Support `python setup.py test`

--- setup.py
+++ setup.py
@@ -123,6 +123,30 @@ my_classifiers = [
 
 install_requires = ['setuptools']
 
+try:
+    from setuptools.command.test import test as TestCommand
+except ImportError:
+    # This setuptools is deprecated so it may be removed in the future.
+    PyTest = None
+else:
+    class PyTest(TestCommand):
+        # user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+        def initialize_options(self):
+            TestCommand.initialize_options(self)
+            self.pytest_args = []
+
+        def finalize_options(self):
+            TestCommand.finalize_options(self)
+            self.test_args = []
+            self.test_suite = True
+
+        def run_tests(self):
+            import pytest
+            errno = pytest.main(self.pytest_args)
+            sys.exit(errno)
+
+
 
 setuptools.setup(
     name='python_toolbox',
@@ -150,5 +174,7 @@ setuptools.setup(
             'docutils>=0.8',
         },
     },
+    cmdclass=({'test': PyTest,} if PyTest is not None else {})
+
 )
 
